bede1bfcae762d5f90f2361ab3e2c40c3380fb7e
danmaninc <unconfigured@null.spigotmc.org>
2021-12-10 22:28:08 +0300

    Added new functions and added new version of config

--- utils/db.js
+++ utils/db.js
@@ -1,32 +1,71 @@
 const sqlite3 = require('sqlite3').verbose();
 const config = require('../config.js')
 module.exports = class db {
-    static add (id, table){
+    static addUser (id) {
         return new Promise((resolve, reject) => {
-            var db = new sqlite3.Database(`${process.env.PATHTOSQLITE}/lazy.db`);
+            var db = new sqlite3.Database(config.databasePath);
             db.serialize(() => {
-                db.run(`INSERT INTO ${table} (discord_id) VALUES (?)`, [id])
-				db.run(`UPDATE ${table} SET (join_date) = ? WHERE discord_id = ${id}`, [Date.now()]);
+                db.run(`INSERT INTO users (discord_id) VALUES (?)`, [id])
+				db.run(`UPDATE users SET (join_date) = ? WHERE discord_id = ${id}`, [Date.now()]);
             })
             db.close()
             resolve()
         });
     }
-    static get (id, table){
+    static addGuild (id) {
         return new Promise((resolve, reject) => {
-            var db = new sqlite3.Database(`${process.env.PATHTOSQLITE}/lazy.db`);
+            var db = new sqlite3.Database(config.databasePath);
             db.serialize(() => {
-                db.all(`SELECT * FROM ${table} WHERE discord_id = ${id}`, (err, row) => {
-                    if(err) console.log(err)
+                db.run(`INSERT INTO guilds (discord_id) VALUES (?)`, [id])
+                db.run (`UPDATE guilds SET (join_date) = ? WHERE discord_id = ${id}`, [Date.now()]);
+            })
+        })
+    }
+    static getUser (id) {
+        return new Promise((resolve, reject) => {
+            var db = new sqlite3.Database(config.databasePath);
+            db.serialize(() => {
+                db.all(`SELECT * FROM users WHERE discord_id = ${id}`, (err, row) => {
+                    if (err) console.log(err)
                     else resolve(row[0]);
                 })
             })
+        })
+    }
+    static getGuild (id) {
+        return new Promise((resolve, reject) => {
+            var db = new sqlite3.Database(config.databasePath);
+            db.serialize(() => {
+                db.all(`SELECT * FROM guilds WHERE discord_id = ${id}`, (err, row) => {
+                    if (err) console.log(err)
+                    else resolve(row[0]);
+                })
+            })
+        })
+    }
+    static changeUser (id, column, new_value) {
+        return new Promise((resolve, reject) => {
+            var db = new sqlite3.Database(config.databasePath);
+            db.serialize(() => {
+                db.run(`UPDATE users SET ${column} = ? WHERE discord_id = ${id}`, [new_value]);
+            });
             db.close()
-        });
+            resolve()
+        })
+    }
+    static changeGuild (id, column, new_value) {
+        return new Promise((resolve, reject) => {
+            var db = new sqlite3.Database(config.databasePath);
+            db.serialize(() => {
+                db.run(`UPDATE guilds SET ${column} = ? WHERE discord_id = ${id}`, [new_value]);
+            });
+            db.close()
+            resolve()
+        })
     }
     static getTable (table) {
         return new Promise((resolve, reject) => {
-            var db = new sqlite3.Database(`${process.env.PATHTOSQLITE}/lazy.db`);
+            var db = new sqlite3.Database(config.databasePath);
             db.serialize(() => {
                 db.all(`SELECT * FROM ${table}`, (err, row) => {
                     if(err) console.log(err)
@@ -34,16 +73,6 @@ module.exports = class db {
                 })
             })
             db.close()
-        })
-    }
-    static change (id, table, column, new_value){
-        return new Promise((resolve, reject) => {
-            var db = new sqlite3.Database(`${process.env.PATHTOSQLITE}/lazy.db`);
-            db.serialize(() => {
-                db.run(`UPDATE ${table} SET ${column} = ? WHERE discord_id = ${id}`, [new_value]);
-            });
-            db.close()
-            resolve()
         })
     }
 }

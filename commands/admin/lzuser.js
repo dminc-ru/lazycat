a5a5691939e96e5b1f0ddef55e9110af9e474f5f
danmaninc <unconfigured@null.spigotmc.org>
2021-12-11 10:19:15 +0300

    lzuser migrated to v13

--- commands/admin/lzuser.js
+++ commands/admin/lzuser.js
@@ -4,47 +4,56 @@ const { MessageEmbed } = require('discord.js')
 module.exports.run = async (client, message, args) => {
 	if (!args[0]) {
         let noInfo = new MessageEmbed()
-		.setColor("#b88fff")
+		.setColor(client.config.embedColor)
 		.setTitle(`Lazy Cat: no info`)
 		.setDescription(`Пользователь не найден.`)
         .setTimestamp()
         .setFooter(`${stats.version}`, client.user.avatarURL()) 
         return message.channel.send(noInfo)
     }
-    let user = await client.users.cache.get(args[0]);
-    let userdb = await client.db.get(args[0], 'users');
-    if (!user || !userdb) {
+    var user = await client.users.fetch(message.mentions.users.first());
+	let noUser = new MessageEmbed()
+		.setColor(client.config.embedColor)
+		.setTitle('Ошибка')
+		.setDescription('Не смог получить ID пользователя.')
+	if (!user) {
+		try {
+		  user = await client.users.fetch(args[0])
+		  if (!user) return message.channel.send(noUser);
+		}
+		catch (error) {
+		  return message.channel.send(noUser);
+		}
+	}
+    let userdb = await client.db.get_user(user.id);
+    if (!userdb) {
         let noInfo = new MessageEmbed()
-		.setColor("#b88fff")
-		.setTitle(`Lazy Cat: no info`)
+		.setColor(client.config.embedColor)
+		.setTitle(`Ошибка`)
 		.setDescription(`Пользователь не найден.`)
-        .setTimestamp()
-        .setFooter(`${stats.version}`, client.user.avatarURL()) 
         return message.channel.send(noInfo)
     }
     let userInfo = new MessageEmbed()
-		.setColor("#b88fff")
+		.setColor(client.config.embedColor)
 		.setTitle(`Lazy Cat: ${user.tag}`)
 		.addField(`Рыбок:`, `${userdb.balance_fish} ${client.emoji.fish}`, true)
         .addField(`Жучков:`, `${userdb.balance_bugs} ${client.emoji.bug}`, true)
         .addField(`Описание:`, `${userdb.description}`, true)
         .addField(`Присоединился:`, `${moment(new Date(Number(userdb.join_date))).locale('ru').format('LLL')}`, false)
-        .addField(`Ежедневная награда:`, `${moment(new Date(Number(userdb.reward_daily))).locale('ru').fromNow()}`, true)
-        .addField(`Еженедельная награда:`, `${moment(new Date(Number(userdb.reward_weekly))).locale('ru').fromNow()}`, true)
+        .addField(`Ежедневная награда:`, `${(Number(userdb.reward_daily) > Date.now()) ? moment(new Date(Number(userdb.reward_daily))).locale('ru').fromNow() : `доступна`}`, true)
+        .addField(`Еженедельная награда:`, `${(Number(userdb.reward_weekly) > Date.now()) ? moment(new Date(Number(userdb.reward_weekly))).locale('ru').fromNow() : `доступна`}`, true)
         .addField(`Математика:`, `${userdb.math_level}`, true)
 		.addField(`Премиум:`, `${(userdb.premium_has == 0) ? 'не активен' : 'активен'}`, true)
         .addField(`Действует до:`, `${(userdb.premium_until == '') ? 'не активен' : `${moment(new Date(Number(userdb.premium_until))).locale('ru').endOf('day').fromNow()}`}`, true)
         .addField(`Работает:`, `${(userdb.work_current == '') ? 'не работает' : `${userdb.work_current}`}`, false)
         .addField(`Текущий ранг:`, `${userdb.work_rank}`, true)
         .addField(`Количество XP:`, `${userdb.work_currentXP}`, true)
-        .setTimestamp()
-        .setFooter(`${stats.version}`, client.user.avatarURL())
+        .setFooter(message.author.tag, message.author.displayAvatarURL({ dynamic: true }))
     message.channel.send(userInfo)
 }
 
-module.exports.help = {
+module.exports.data = {
     name: "lzuser",
-    aliases: [],
     permissions: ["lia"],
-    modules: ["admin"]
+    type: "message"
 }

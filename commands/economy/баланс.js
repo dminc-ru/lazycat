acd41a761c1c9b13f42bb26c1f37eb78c56173ee
Dan <whatdanman@gmail.com>
2021-12-11 15:41:27 +0300

    Код команды «баланс» обновлён до v13

--- commands/economy/баланс.js
+++ commands/economy/баланс.js
@@ -1,49 +1,16 @@
 module.exports.run = async (client, interaction) => {
-	try{
-		let user = client.users.cache.get(interaction.member.user.id);
-		let userdb = await client.db.get(interaction.member.user.id, 'users')
-		client.api.interactions(interaction.id, interaction.token).callback.post({
-			data: {
-				type: 4,
-				data: {
-					embeds: [
-						{
-							color: 0xb88fff,
-							title: 'Баланс',
-							author: {
-								name: `${user.tag}`,
-								icon_url: `${user.displayAvatarURL()}`
-							},
-							fields: [
-								{
-									name: 'Рыбки:',
-									value: `${userdb.balance_fish} <:lz_fish:742459590087803010>`,
-									inline: true
-								},
-								{
-									name: 'Жучки:',
-									value: `${userdb.balance_bugs} <:lz_bug:742039591929905223>`,
-									inline: true
-								},
-								{
-									name: 'В банке:',
-									value: `${userdb.balance_bank} <:lz_fish:742459590087803010>`,
-									inline: false
-								}
-							],
-							timestamp: new Date(),
-							footer: {
-								text: `${user.tag}`,
-								icon_url: `${user.displayAvatarURL()}`,
-							}
-						}
-					]
-				}
-			}
-		});
-	}catch(error){
-		client.logger.log(`${error}`, "err");
-	}
+	let user = await client.users.fetch(interaction.member.user.id);
+	let userdb = await client.db.get(interaction.member.user.id, 'users')
+	let userProfile = new MessageEmbed()
+		.setColor(client.config.embedColor)
+		.setTitle('Баланс')
+		.setAuthor(user.tag, user.displayAvatarURL({dynamic: true}))
+		.addField(`Рыбки:`, `${userdb.balance_fish} ${client.emoji.fish}`, true)
+		.addField(`Жучки:`, `${userdb.balance_bugs} ${client.emoji.bug}`, true)
+		.addField(`В банке:`, `${userdb.balance_bank} ${client.emoji.fish}`, false)
+		.setTimestamp()
+		.setFooter(user.tag, user.displayAvatarURL({dynamic: true}))
+	interaction.reply({embeds: [userProfile]})
 }
 
 module.exports.help = {

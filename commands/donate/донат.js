b3931d5b926aafdf5b1510957bfef1e522dd9929
Dan <whatdanman@gmail.com>
2021-12-11 12:58:47 +0300

    Код доната обновлён до v13

--- commands/donate/донат.js
+++ commands/donate/донат.js
@@ -1,39 +1,17 @@
 module.exports.run = async (client, interaction) => {
-	try{
-		let user = client.users.cache.get(interaction.member.user.id);
-		client.api.interactions(interaction.id, interaction.token).callback.post({
-			data: {
-				type: 4,
-				data: {
-					embeds: [
-						{
-							color: 0xb88fff,
-							title: '<:lz_heart:818202697530474567> Донат',
-							description: `Здесь вы можете материально поддержать разработку Lazy Cat. Спасибо!`,
-							fields: [
-								{
-									name: '<:da:818204628843233353> Donation Alerts',
-									value: '[клик](https://www.donationalerts.com/r/lazycatru)',
-								}
-							],
-							timestamp: new Date(),
-							footer: {
-								text: `${user.tag}`,
-								icon_url: `${user.displayAvatarURL()}`,
-							}
-						}
-					]
-				}
-			}
-		});
-	}catch(error){
-		client.logger.log(`${error}`, "err");
-	}
+		let user = await client.users.fetch(interaction.member.user.id);
+		let donateEmbed = new MessageEmbed()
+			.setColor(client.config.embedColor)
+			.setTitle(`${client.emoji.heart} Донат`)
+			.setDescription(`Здесь вы можете материально поддержать разработку Lazy Cat. Спасибо!`)
+			.addField(`${client.emoji.da} Donation Alerts`, `[клик](${client.config.daLink})`)
+			.setTimestamp()
+			.setFooter(user.tag, user.displayAvatarURL({dynamic: true}))
+		interaction.reply({embeds: [donateEmbed]})
 }
 
-module.exports.help = {
+module.exports.data = {
 	name: "донат",
-	aliases: ["ljyfn"],
 	permissions: ["member"],
-	modules: ["donate"]
+	type: "interaction"
 }

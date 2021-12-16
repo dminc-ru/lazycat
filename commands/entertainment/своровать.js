4da11e273bf4920296e6df48bae5f4948aeed632
Dan <whatdanman@gmail.com>
2021-12-16 15:46:38 +0300

    Код команды обновлён до v13

--- commands/entertainment/своровать.js
+++ commands/entertainment/своровать.js
@@ -1,111 +1,47 @@
 module.exports.run = async (client, interaction) => {
-try{
-	let user = client.users.cache.get(interaction.member.user.id);
+	let user = await client.users.fetch(interaction.member.user.id);
 	let member = interaction.data.options[0].value;
 	function randomSteal(min, max) {
 		let rand = min - 0.5 + Math.random() * (max - min + 1);
 		return Math.round(rand);
 	}
-	let memberdb = await client.db.get(interaction.member.user.id, 'users')
-	let userdb = await client.db.get(interaction.member.user.id, 'users')
-	if(member == interaction.member.user.id){
-		return client.api.interactions(interaction.id, interaction.token).callback.post({
-			data: {
-				type: 4,
-				data: {
-					flags: 64,
-					content: `Вы не можете своровать рыбки у самого себя.`
-				}
-			}
-		});
-	}
-	if(!memberdb){
-		return client.api.interactions(interaction.id, interaction.token).callback.post({
-			data: {
-				type: 4,
-				data: {
-					flags: 64,
-					content: `Указанный пользователь не зарегистрирован в системе Lazy Cat.`
-				}
-			}
-		});
-	}
-	if(memberdb.balance_fish < 50){
-		return client.api.interactions(interaction.id, interaction.token).callback.post({
-			data: {
-				type: 4,
-				data: {
-					flags: 64,
-					content: `Этот пользователь не имеет так много рыбок, чтобы его можно было обокрасть.`
-				}
-			}
-		});
-	}
+	let memberdb = await client.db.getUser(interaction.member.user.id)
+	let userdb = await client.db.getUser(interaction.member.user.id)
+	if(member == interaction.member.user.id)
+		return interaction.reply({content: "Вы не можете своровать рыбки у самого себя.", ephemeral: true})
+	if(!memberdb)
+		return interaction.reply({content: "Указанный пользователь не зарегистрирован в системе Lazy Cat.", ephemeral: true})
+	if(memberdb.balance_fish < 50)
+		return interaction.reply({content: "Этот пользователь не имеет так много рыбок, чтобы его можно было обокрасть.", ephemeral: true})
 	if(member == 707539807957680129){
-		return client.api.interactions(interaction.id, interaction.token).callback.post({
-			data: {
-				type: 4,
-				data: {
-					flags: 64,
-					content: `Внимательный котик вас заметил, а вы его нет. Кажется, это бесполезно.`
-				}
-			}
-		});
+		return interaction.reply({content: "Внимательный котик вас заметил, а вы его нет. Кажется, это бесполезно.", ephemeral: true})
 	}
 	let tryin = randomSteal(1, 100)
 	if(tryin < 30){
-		return client.api.interactions(interaction.id, interaction.token).callback.post({
-			data: {
-				type: 4,
-				data: {
-					embeds: [
-						{
-							color: 0xb88fff,
-							title: 'Воровство: неудача',
-							description: `<:lz_fish:742459590087803010> Неудача! Вас заметили, и вы пытаетесь скрыться.`,
-							timestamp: new Date(),
-							footer: {
-								text: `${user.tag}`,
-								icon_url: `${user.displayAvatarURL()}`,
-							}
-						}
-					]
-				}
-			}
-		});
+		let failEmbed = new MessageEmbed()
+			.setColor(client.config.embedColor)
+			.setTitle('Воровство: неудача')
+			.setDescription(`${client.emoji.fish} Неудача! Вас заметили, и вы пытаетесь скрыться.`)
+			.setTimestamp()
+			.setFooter(user.tag, user.displayAvatarURL({dynamic: true}))
+		return interaction.reply({embeds: [failEmbed]})
 	}
 	else{
 		let stFish = randomSteal(1, 5)
-		client.db.change(memberdb.discord_id, 'users', 'balance_fish', (memberdb.balance_fish - stFish))
-		client.db.change(interaction.member.user.id, 'users', 'balance_fish', (userdb.balance_fish + stFish))
-		return client.api.interactions(interaction.id, interaction.token).callback.post({
-			data: {
-				type: 4,
-				data: {
-					embeds: [
-						{
-							color: 0xb88fff,
-							title: 'Воровство: успешно',
-							description: `<:lz_fish:742459590087803010> Шалость удалась! Вам зачислено ${stFish} <:lz_fish:742459590087803010>.`,
-							timestamp: new Date(),
-							footer: {
-								text: `${user.tag}`,
-								icon_url: `${user.displayAvatarURL()}`,
-							}
-						}
-					]
-				}
-			}
-		});
+		client.db.changeUser(memberdb.discord_id, 'balance_fish', (memberdb.balance_fish - stFish))
+		client.db.changeUser(interaction.member.user.id, 'balance_fish', (userdb.balance_fish + stFish))
+		let successEmbed = new MessageEmbed()
+			.setColor(client.config.embedColor)
+			.setTitle('Воровство: успешно')
+			.setDescription(`${client.emoji.fish} Шалость удалась! Вам зачислено ${stFish} ${client.emoji.fish}`)
+			.setTimestamp()
+			.setFooter(user.tag, user.displayAvatarURL({dynamic: true}))
+		return interaction.reply({embeds: [successEmbed]})
 	};
-}catch(error){
-		client.logger.log(`${error}`, "err");
-	}
 }
 
-module.exports.help = {
+module.exports.data = {
 	name: "своровать",
-	aliases: ["cdjhjdfnm", "украсть", "erhfcnm"],
 	permissions: ["member"],
-	modules: ["entertainment"]
+	type: "interaction"
 }

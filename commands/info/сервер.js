a2155fd3796f7070847ec16f595e5cd7fdefd5f7
Dan <whatdanman@gmail.com>
2021-12-16 17:03:57 +0300

    Код команды обновлён до v13

--- commands/info/сервер.js
+++ commands/info/сервер.js
@@ -1,132 +1,64 @@
 let moment = require('moment');
 module.exports.run = async (client, interaction) => {
-	try{
-		let user = client.users.cache.get(interaction.member.user.id);
-		let guild = client.guilds.cache.get(interaction.guild_id);
-		function checkDays(date) {
-			let now = new Date();
-			let diff = now.getTime() - date.getTime();
-			let days = Math.floor(diff / 86400000);
-			return days + (days == 1 ? " день" : " дн.") + " назад";
-		};
-		let verifLevels = {
-			"NONE": "-", 
-			"LOW": "Низкий", 
-			"MEDIUM": "Средний", 
-			"HIGH": "Высокий", 
-			"VERY_HIGH": "Самый высокий"
-		};
-		let region = {
-			"brazil": ":flag_br: Бразилия",
-			"europe": ":flag_eu: Европа",
-			"singapore": ":flag_sg: Сингапур",
-			"us-central": ":flag_us: Центральная Америка",
-			"sydney": ":flag_au: Сидней",
-			"us-east": ":flag_us: Восточная Америка",
-			"us-south": ":flag_us: Южная Америка",
-			"us-west": ":flag_us: Западная Америка",
-			"eu-west": ":flag_eu: Западная Европа",
-			"vip-us-east": ":flag_us: VIP Восточная Америка",
-			"london": ":flag_gb: Лондон",
-			"amsterdam": ":flag_nl: Амстердам",
-			"hongkong": ":flag_hk: Гонконг",
-			"russia": ":flag_ru: Россия",
-			"southafrica": ":flag_za:  Южная Африка",
-			"india": ":flag_in: Индия",
-			"japan": ":flag_jp: Япония"
-		};
-		moment.locale("ru");
-		const servercreated = moment(guild.createdAt).format('LL');
-		return client.api.interactions(interaction.id, interaction.token).callback.post({
-			data: {
-				type: 4,
-				data: {
-					embeds: [
-						{
-							color: 0xb88fff,
-							title: `Информация о сервере ${guild.name}`,
-							thumbnail: {
-								url: `${guild.iconURL()}`
-							},
-							fields: [
-								{
-									name: "ID",
-									value: `${interaction.guild_id}`,
-									inline: true
-								},
-								{
-									name: "Владелец",
-									value: `<@${guild.owner.user.id}>`,
-									inline: true
-								},
-								{
-									name: "Регион",
-									value: `${region[guild.region]}`,
-									inline: true
-								},
-								{
-									name: "Всего участников",
-									value: `${guild.memberCount}`,
-									inline: true
-								},
-								{
-									name: "Людей",
-									value: `${guild.members.cache.filter(member => !member.user.bot).size}`,
-									inline: true
-								},
-								{
-									name: "Ботов",
-									value: `${guild.members.cache.filter(member => member.user.bot).size}`,
-									inline: true
-								},
-								{
-									name: "Уровень верификации",
-									value: `${verifLevels[guild.verificationLevel]}`,
-									inline: true
-								},
-								{
-									name: "Всего каналов",
-									value: `${guild.channels.cache.size}`,
-									inline: true
-								},
-								{
-									name: "Всего ролей",
-									value: `${guild.roles.cache.size}`,
-									inline: true
-								},
-								{
-									name: "Дата создания",
-									value: `${servercreated} (${checkDays(guild.createdAt)})`,
-									inline: true
-								},
-								{
-									name: "Всего бустов",
-									value: `${guild.premiumSubscriptionCount}`,
-									inline: true
-								},
-								{
-									name: "Уровень буста",
-									value: `${guild.premiumTier}`
-								}
-							],
-							timestamp: new Date(),
-							footer: {
-								text: `${user.tag}`,
-								icon_url: `${user.displayAvatarURL()}`,
-							}
-						}
-					]
-				}
-			}
-		});
-	}catch(error){
-		client.logger.log(`${error}`, "err");
-	}
+	let user = await client.users.fetch(interaction.member.user.id);
+	let guild = await client.guilds.fetch(interaction.guild_id);
+	function checkDays(date) {
+		let now = new Date();
+		let diff = now.getTime() - date.getTime();
+		let days = Math.floor(diff / 86400000);
+		return days + (days == 1 ? " день" : " дн.") + " назад";
+	};
+	let verifLevels = {
+		"NONE": "-", 
+		"LOW": "Низкий", 
+		"MEDIUM": "Средний", 
+		"HIGH": "Высокий", 
+		"VERY_HIGH": "Самый высокий"
+	};
+	let region = {
+		"brazil": ":flag_br: Бразилия",
+		"europe": ":flag_eu: Европа",
+		"singapore": ":flag_sg: Сингапур",
+		"us-central": ":flag_us: Центральная Америка",
+		"sydney": ":flag_au: Сидней",
+		"us-east": ":flag_us: Восточная Америка",
+		"us-south": ":flag_us: Южная Америка",
+		"us-west": ":flag_us: Западная Америка",
+		"eu-west": ":flag_eu: Западная Европа",
+		"vip-us-east": ":flag_us: VIP Восточная Америка",
+		"london": ":flag_gb: Лондон",
+		"amsterdam": ":flag_nl: Амстердам",
+		"hongkong": ":flag_hk: Гонконг",
+		"russia": ":flag_ru: Россия",
+		"southafrica": ":flag_za:  Южная Африка",
+		"india": ":flag_in: Индия",
+		"japan": ":flag_jp: Япония"
+	};
+	moment.locale("ru");
+	const servercreated = moment(guild.createdAt).format('LL');
+	let serverEmbed = new MessageEmbed()
+		.setColor(client.config.embedColor)
+		.setTitle(`Информация о сервере ${guild.name}`)
+		.setThumbnail(guild.iconURL())
+		.addField('ID', interaction.guild_id, true)
+		.addField('Владелец', `<@${guild.owner.user.id}>`, true)
+		.addField('Регион', region[guild.region], true)
+		.addField('Всего участников', guild.memberCount, true)
+		.addField('Людей', guild.members.cache.filter(member => !member.user.bot).size, true)
+		.addField('Ботов', guild.members.cache.filter(member => member.user.bot).size, true)
+		.addField('Уровень верификации', verifLevels[guild.verificationLevel], true)
+		.addField('Всего каналов', guild.channels.cache.size, true)
+		.addField('Всего ролей', guild.roles.cache.size, true)
+		.addField('Дата создания', `${servercreated} (${checkDays(guild.createdAt)})`, true)
+		.addField('Всего бустов', guild.premiumSubscriptionCount, true)
+		.addField('Уровень буста', guild.premiumTier)
+		.setTimestamp()
+		.setFooter(user.tag, user.displayAvatarURL({dynamic: true}))
+	return interaction.reply({embeds: [serverEmbed]})
 }
 
-module.exports.help = {
+module.exports.data = {
 	name: "сервер",
-	aliases: ["cthdth"],
 	permissions: ["member"],
-	modules: ["info"]
+	type: "interaction"
 }

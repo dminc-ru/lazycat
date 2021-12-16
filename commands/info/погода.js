51905ae2ba3f968080fc6c8c8dbd08c99c90f4ed
Dan <whatdanman@gmail.com>
2021-12-16 16:34:37 +0300

    Код команды обновлён до v13

--- commands/info/погода.js
+++ commands/info/погода.js
@@ -1,23 +1,13 @@
 const weather = require('openweather-apis');
 module.exports.run = async (client, interaction) => {
-	try{
 		let user = client.users.cache.get(interaction.member.user.id);
-		weather.setAPPID('06e6a1224c53e468f618b7497088a742');
+		weather.setAPPID(client.config.weatherID);
 		weather.setLang('ru');
 		weather.setUnits('metric');		
 		weather.setCity(interaction.data.options[0].value);
 		weather.getAllWeather(function(err, response){
-			if(err) {
-				return client.api.interactions(interaction.id, interaction.token).callback.post({
-							data: {
-								type: 4,
-								data: {
-									flags: 64,
-									content: "Укажите корректное местоположение."
-								}
-							}
-						});
-			}
+			if(err)
+				return interaction.reply({content: "Укажите корректное местоположение.", ephemeral: true})
 			let shift = response.timezone / 60;
 			shift = shift / 60;
 			if(shift > 0){
@@ -27,74 +17,25 @@ module.exports.run = async (client, interaction) => {
 			};
 			let currentTemp = Math.floor(response.main.temp);
 			let feelsLike = Math.floor(response.main.feels_like);
-			return client.api.interactions(interaction.id, interaction.token).callback.post({
-				data: {
-					type: 4,
-					data: {
-						embeds: [
-							{
-								color: 0xb88fff,
-								timestamp: new Date(),
-								thumbnail: {
-									url: `http://openweathermap.org/img/wn/${response.weather[0].icon}@2x.png`
-								},
-								title: `Погода: ${response.name}`,
-								fields: [
-									{
-										name: 'Часовой пояс:',
-										value: `UTC${sign}${shift}`,
-										inline: false,
-									},
-									{
-										name: 'Температура:',
-										value: `${currentTemp}°C`,
-										inline: true,
-									},
-									{
-										name: 'Погода:',
-										value: `${response.weather[0].description}`,
-										inline: true,
-									},
-									{
-										name: 'Ветер:',
-										value: `${response.wind.speed} м/с`,
-										inline: true,
-									},
-									{
-										name: 'Ощущается как:',
-										value: `${feelsLike}°C`,
-										inline: true,
-									},
-									{
-										name: 'Влажность:',
-										value: `${response.main.humidity}%`,
-										inline: true,
-									},
-									{
-										name: 'Облачность:',
-										value: `${response.clouds.all}%`,
-										inline: true,
-									},
-								],
-								footer: {
-									text: `${user.tag}`,
-									icon_url: `${user.displayAvatarURL()}`,
-								}
-							}
-						]
-					}
-				}
-			});
+			let weatherEmbed = new MessageEmbed()
+				.setColor(client.config.embedColor)
+				.setTitle(`Погода: ${response.name}`)
+				.setThumbnail(`http://openweathermap.org/img/wn/${response.weather[0].icon}@2x.png`)
+				.addField(`Часовой пояс:`, `UTS${sign}${shift}`, false)
+				.addField('Температура:', `${currentTemp}°C`, true)
+				.addField('Погода', response.weather[0].description, true)
+				.addField('Ветер:', `${response.wind.speed} м/с`, true)
+				.addField('Ощущается как:', `${feelsLike}°C`, true)
+				.addField('Влажность:', `${response.main.humidity}%`, true)
+				.addField('Облачность:', `${response.clouds.all}%`, true)
+				.setTimestamp()
+				.setFooter(user.tag, user.displayAvatarURL({dynamic: true}))
+			return interaction.reply({embeds: [weatherEmbed]})
 		});
-	
-	}catch(error){
-		client.logger.log(`${error}`, "err");
-		}
 }
 
-module.exports.help = {
+module.exports.data = {
 	name: "погода",
-	aliases: ["gjujlf"],
 	permissions: ["member"],
-	modules: ["info"]
+	type: "interaction"
 }

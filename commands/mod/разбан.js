8c7c1f92ffde4a8fce6c74f3a8c559ec245e1067
Dan <whatdanman@gmail.com>
2021-12-23 17:37:58 +0300

    Код команды обновлён до v13

--- commands/mod/разбан.js
+++ commands/mod/разбан.js
@@ -1,133 +1,68 @@
 const { MessageEmbed } = require("discord.js");
 module.exports.run = async (client, interaction) => {
-try{
-	let user = client.users.cache.get(interaction.member.user.id);
-	let guild = client.guilds.cache.get(interaction.guild_id);
-	let guilddb = await client.db.get(interaction.guild_id, 'guilds')
-	let member = guild.members.cache.get(interaction.member.user.id);
+	let user = await client.users.fetch(interaction.member.user.id);
+	let guild = await client.guilds.fetch(interaction.guild_id);
+	let guilddb = await client.db.getGuild(interaction.guild_id)
+	let member = await guild.members.fetch(interaction.member.user.id);
 	let toUnban = interaction.data.options[0].value;
-	var toUnbanClientResolve = client.users.cache.get(toUnban);
-	if(!member.hasPermission('BAN_MEMBERS'))
-	return client.api.interactions(interaction.id, interaction.token).callback.post({
-		data: {
-			type: 4,
-			data: {
-				flags: 64,
-				content: `У вас недостаточно прав для выполнения этой команды.`
-			}
-		}
-	});
-		const banList = await guild.fetchBans();
-		var bannedUser = banList.find(us => us.user.id === toUnban);
-		if(!bannedUser){
-			bannedUser = banList.find(us => us.user.tag === toUnban);
-			if(!bannedUser){
-				return client.api.interactions(interaction.id, interaction.token).callback.post({
-					data: {
-						type: 4,
-						data: {
-							flags: 64,
-							content: `Пользователь не найден.`
-						}
-					}
-				});
-			}
-		}
-		try {guild.members.unban(bannedUser.user.id, {reason});}catch (error) {return client.api.interactions(interaction.id, interaction.token).callback.post({
-			data: {
-				type: 4,
-				data: {
-					flags: 64,
-					content: `Произошла ошибка при попытке разбана. Возможно, у меня недостаточно прав для выполнения этого действия.`
-				}
-			}
-		});}
-	if(interaction.data.options.length > 1)
-		var reason = interaction.data.options[1].value
-	else
-		var reason = `не указана`;
-	if(toUnban == interaction.member.user.id){
-		return client.api.interactions(interaction.id, interaction.token).callback.post({
-			data: {
-				type: 4,
-				data: {
-					flags: 64,
-					content: `Вы не можете разбанить себя.`
-				}
-			}
-		});
+	var toUnbanClientResolve = await client.users.fetch(toUnban);
+	if( !member.hasPermission('BAN_MEMBERS') ) {
+		return interaction.reply({content: `У вас недостаточно прав для выполнения этой команды.`, ephemeral: true})
 	}
-	if(toUnban == client.user.id)
-	return client.api.interactions(interaction.id, interaction.token).callback.post({
-		data: {
-			type: 4,
-			data: {
-				flags: 64,
-				content: `У вас недостаточно прав для выполнения данного действия.`
-			}
+	const banList = await guild.fetchBans();
+	var bannedUser = banList.find(us => us.user.id === toUnban);
+	if (!bannedUser) {
+		bannedUser = banList.find(us => us.user.tag === toUnban);
+		if(!bannedUser){
+			return interaction.reply({content: `Пользователь не найден.`, ephemeral: true})
 		}
-	});
+	}
+	try {
+		guild.members.unban(bannedUser.user.id, {reason});
+	} catch (error) {
+		return interaction.reply({content: `Произошла ошибка при попытке разбана. Возможно, у меня недостаточно прав для выполнения этого действия.`, ephemeral: true})
+	}
+	if (interaction.data.options.length > 1) {
+		var reason = interaction.data.options[1].value
+	} else {
+		var reason = `не указана`;
+	}
+	if (toUnban == interaction.member.user.id) {
+		return interaction.reply({content: `Вы не можете разбанить себя.`, ephemeral: true})
+	}
+	if (toUnban == client.user.id) {
+		return interaction.reply({content: `У вас недостаточно прав для выполнения данного действия.`, ephemeral: true})
+	}
 	let usernames = toUnbanClientResolve.tag;
-		client.api.interactions(interaction.id, interaction.token).callback.post({
-			data: {
-				type: 4,
-				data: {
-					embeds: [
-						{
-							color: 0xb88fff,
-							title: 'Разбан: успешно',
-							fields: [
-								{
-									name: "Модератор:",
-									value: `${user.tag}`,
-									inline: true
-								},
-								{
-									name: "Пользователь:",
-									value: `${usernames}`,
-									inline: true
-								},
-								{
-									name: "Причина:",
-									value: `${reason}`,
-									inline: false
-								},
-							],
-							timestamp: new Date(),
-							footer: {
-								text: `${user.tag}`,
-								icon_url: `${user.displayAvatarURL()}`,
-							}
-						}
-					]
-				}
-			}
-		});
-	if(guilddb.logmsg_channel != ""){
+	let unbanSuccess = new MessageEmbed()
+		.setColor(client.config.embedColor)
+		.setTitle('Разбан: успешно')
+		.addField('Модератор:', user.tag, true)
+		.addField('Пользователь:', usernames, true)
+		.addField('Причина:', reason, false)
+		.setTimestamp()
+		.setFooter(user.tag, user.displayAvatarURL({dynamic: true}))
+	interaction.reply({embeds: [unbanSuccess]})
+	if (guilddb.logmsg_channel != "") {
 		try{
-		let banMessage = new MessageEmbed()
-				.setColor("#b88fff")
+			let banMessage = new MessageEmbed()
+				.setColor(client.config.embedColor)
 				.setTitle(`Разбан: успешно`)
 				.addField(`Модератор:`, `${user.tag}`, true)
 				.addField(`Пользователь:`, `${usernames}`, true)
 				.addField(`Причина:`, `${reason}`, false)
 				.setTimestamp()
-				.setFooter(`${user.tag}`, user.avatarURL())
-		const channel = guild.channels.cache.get(guilddb.logmsg_channel);
-		channel.send(banMessage);
+				.setFooter(`${user.tag}`, user.displayAvatarURL({dynamic: true}))
+			const channel = await guild.channels.fetch(guilddb.logmsg_channel);
+			channel.send(banMessage);
 		}catch(error){
-			return client.db.change(interaction.guild_id, 'guilds', 'logmsg_channel', '')
+			return client.db.changeGuild(interaction.guild_id, 'logmsg_channel', '')
 		}
-	}
-
-}catch(error){
-		client.logger.log(`${error}`, "err");
 	}
 }
 
-module.exports.help = {
+module.exports.data = {
 	name: "разбан",
-	aliases: ["hfp,fy"],
 	permissions: ["member"],
-	modules: ["mod"]
+	type: "interaction"
 }

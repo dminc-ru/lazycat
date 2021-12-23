83d2827ededaa237b920f1ca945d7713d21fcd89
Dan <whatdanman@gmail.com>
2021-12-23 18:26:57 +0300

    Код обновлён

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -1,14 +1,16 @@
 module.exports = async (client, member) => {
-	let guilddb = await client.db.get(member.guild.id, 'guilds')
+	let guilddb = await client.db.getGuild(member.guild.id)
     if(guilddb.welcomeChannel != ''){
-		const channel = member.guild.channels.cache.get(guilddb.welcomeChannel);
+		const channel = await member.guild.channels.fetch(guilddb.welcomeChannel);
 		channel.send(`<@${member.id}> ${guilddb.welcomeText}`);
 	}
 	if(guilddb.giveRole != 'false'){
 		const role = member.guild.roles.cache.find(role => role.id === guilddb.welcomeRole);
 		let reason = `Выдача приветственной роли (/стартроль)`
 		try{
-		member.roles.add(role, {reason});
-		}catch(error){return;}
+			member.roles.add(role, {reason});
+		} catch(error) {
+			return;
+		}
 	}
 };

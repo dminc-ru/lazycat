5f0cebc6d99253a574d41137337db6306b57d765
Dan <whatdanman@gmail.com>
2021-12-23 11:45:02 +0300

    Код команды обновлён до v13

--- commands/mod/бан.js
+++ commands/mod/бан.js
@@ -1,119 +1,52 @@
 const { MessageEmbed } = require("discord.js");
 const fs = require("fs");
-let bans = require(`${process.env.PATHTOBASE}/bans.json`);
+let bans = require(`${client.config.jsonPath}bans.json`);
 module.exports.run = async (client, interaction) => {
-	try{
-	let user = client.users.cache.get(interaction.member.user.id);
-	let guild = client.guilds.cache.get(interaction.guild_id);
-	let guilddb = await client.db.get(interaction.guild_id, 'guilds')
-	let member = guild.members.cache.get(interaction.member.user.id);
-	if(!member.hasPermission('BAN_MEMBERS'))
-		return client.api.interactions(interaction.id, interaction.token).callback.post({
-			data: {
-				type: 4,
-				data: {
-					flags: 64,
-					content: `У вас недостаточно прав для выполнения этой команды.`
-				}
-			}
-		});
+	let user = await client.users.fetch(interaction.member.user.id);
+	let guild = await client.guilds.fetch(interaction.guild_id);
+	let guilddb = await client.db.getGuild(interaction.guild_id)
+	let member = await guild.members.fetch(interaction.member.user.id);
+	if (!member.hasPermission('BAN_MEMBERS')) {
+		return interaction.reply({content: `У вас недостаточно прав для выполнения этой команды.`, ephemeral: true})
+	}
 	var banUser = interaction.data.options[0].value;
 	var banUserResolve = guild.members.fetch(banUser);
-	if(!banUserResolve)
-		return client.api.interactions(interaction.id, interaction.token).callback.post({
-			data: {
-				type: 4,
-				data: {
-					flags: 64,
-					content: `Пользователь не найден.`
-				}
-			}
-		});
-			const memberPosition = banUserResolve.roles.highest.position;
-			const moderationPosition = member.roles.highest.position;
-			if(guild.owner.user.id != banUser && moderationPosition < memberPosition){
-				return client.api.interactions(interaction.id, interaction.token).callback.post({
-					data: {
-						type: 4,
-						data: {
-							flags: 64,
-							content: `У вас недостаточно прав для выполнения этой команды.`
-						}
-					}
-				});
-			}
-			if(!banUserResolve.bannable) {
-				return client.api.interactions(interaction.id, interaction.token).callback.post({
-					data: {
-						type: 4,
-						data: {
-							flags: 64,
-							content: `У вас недостаточно прав для выполнения этой команды.`
-						}
-					}
-				});
-			}
-	if(banUser == interaction.member.user.id)
-		return client.api.interactions(interaction.id, interaction.token).callback.post({
-			data: {
-				type: 4,
-				data: {
-					flags: 64,
-					content: `Вы не можете забанить себя.`
-				}
-			}
-		});
+	if (!banUserResolve) {
+		return interaction.reply({content: `Пользователь не найден.`, ephemeral: true})
+	}
+	const memberPosition = banUserResolve.roles.highest.position;
+	const moderationPosition = member.roles.highest.position;
+	if (guild.owner.user.id != banUser && moderationPosition < memberPosition) {
+		return interaction.reply({content: `У вас недостаточно прав для выполнения этой команды.`, ephemeral: true})
+	}
+	if (!banUserResolve.bannable) {
+		return interaction.reply({content: `У вас недостаточно прав для выполнения этой команды.`, ephemeral: true})
+	}
+	if (banUser == interaction.member.user.id) {
+		return interaction.reply({content: `Вы не можете забанить себя.`, ephemeral: true})
+	}
 	const banned = await guild.fetchBans();
-		if(banned.some((m) => m.user.id === banUser)){
-			return client.api.interactions(interaction.id, interaction.token).callback.post({
-				data: {
-					type: 4,
-					data: {
-						flags: 64,
-						content: `Пользователь уже забанен.`
-					}
-				}
-			});
+		if (banned.some((m) => m.user.id === banUser)) {
+			return interaction.reply({content: `Пользователь уже забанен.`, ephemeral: true})
 		}
-	if(interaction.data.options.length > 1)
+	if (interaction.data.options.length > 1) {
 		var reason = interaction.data.options[1].value
-	else
+	} else {
 		var reason = `не указана`
-	if(reason.length > 500) 
-		return client.api.interactions(interaction.id, interaction.token).callback.post({
-			data: {
-				type: 4,
-				data: {
-					flags: 64,
-					content: `Максимальное количество символов для причины — 500.`
-				}
-			}
-		});
+	}
+	if (reason.length > 500) {
+		return interaction.reply({content: `Максимальное количество символов для причины — 500.`, ephemeral: true})
+	}
 	let usernames = banUserResolve.tag;
-	if(banUser == client.user.id)
-		return client.api.interactions(interaction.id, interaction.token).callback.post({
-			data: {
-				type: 4,
-				data: {
-					flags: 64,
-					content: `У вас недостаточно прав для выполнения данного действия.`
-				}
-			}
-		});
+	if (banUser == client.user.id) {
+		return interaction.reply({content: `У вас недостаточно прав для выполнения данного действия.`, ephemeral: true})
+	}
 	try {
 		banUserResolve.ban(reason);
 	} catch (error) {
-		return client.api.interactions(interaction.id, interaction.token).callback.post({
-			data: {
-				type: 4,
-				data: {
-					flags: 64,
-					content: `Произошла ошибка при попытке бана. Возможно, у меня недостаточно прав для выполнения этого действия.`
-				}
-			}
-		});
+		return interaction.reply({content: `Произошла ошибка при попытке бана. Возможно, у меня недостаточно прав для выполнения этого действия.`})
 	}
-	if(!bans[interaction.guild_id]){
+	if (!bans[interaction.guild_id]) {
 		bans[interaction.guild_id] = [];
 	}
 	bans[interaction.guild_id].push({
@@ -125,42 +58,16 @@ module.exports.run = async (client, interaction) => {
 		reason: reason,
 		banCreatedAt: Date.now()
 	});
-	fs.writeFileSync(`${process.env.PATHTOBASE}/bans.json`, JSON.stringify(bans, null, "\t"));
-		client.api.interactions(interaction.id, interaction.token).callback.post({
-			data: {
-				type: 4,
-				data: {
-					embeds: [
-						{
-							color: 0xb88fff,
-							title: 'Бан: успешно',
-							fields: [
-								{
-									name: "Модератор:",
-									value: `${user.tag}`,
-									inline: true
-								},
-								{
-									name: "Пользователь:",
-									value: `${banUserResolve.tag}`,
-									inline: true
-								},
-								{
-									name: "Причина:",
-									value: `${reason}`,
-									inline: false
-								},
-							],
-							timestamp: new Date(),
-							footer: {
-								text: `${user.tag}`,
-								icon_url: `${user.displayAvatarURL()}`,
-							}
-						}
-					]
-				}
-			}
-		});
+	fs.writeFileSync(`${client.config.jsonPath}bans.json`, JSON.stringify(bans, null, "\t"));
+	let banMessage = new MessageEmbed()
+		.setColor(client.config.embedColor)
+		.setTitle('Бан: успешно')
+		.addField(`Модератор:`, user.tag, true)
+		.addField(`Пользователь:`, banUserResolve.tag, true)
+		.addField(`Причина:`, reason, false)
+		.setTimestamp()
+		.setFooter(user.tag, user.displayAvatarURL({dynamic: true}))
+	interaction.reply({embeds: [banMessage]})
 	if(guilddb.logmsg_channel != ""){
 		try{
 			let banMessage = new MessageEmbed()
@@ -174,18 +81,13 @@ module.exports.run = async (client, interaction) => {
 			const channel = guild.channels.fetch(guilddb.logmsg_channel);
 			channel.send(banMessage);
 		}catch(error){
-			client.db.change(interaction.guild_id, 'guilds', 'logmsg_channel', '')
+			client.db.changeGuild(interaction.guild_id, 'logmsg_channel', '')
 		}
-	}
-
-	}catch(error){
-		client.logger.log(`${error}`, "err");
 	}
 }
 
-module.exports.help = {
+module.exports.data = {
 	name: "бан",
-	aliases: [",fy"],
 	permissions: ["member"],
-	modules: ["mod"]
+	type: "interaction"
 }

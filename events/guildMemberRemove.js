a37142a8561239a3980095de98170537d983082a
Dan <whatdanman@gmail.com>
2021-12-23 18:26:17 +0300

    Код обновлён

--- events/guildMemberRemove.js
+++ events/guildMemberRemove.js
@@ -1,7 +1,7 @@
 module.exports = async (client, member) => {
-	let guilddb = client.db.get(member.guild.id, 'guilds')
+	let guilddb = client.db.getGuild(member.guild.id)
     if(guilddb.goodbyeChannel == '') return;
-	const channel = member.guild.channels.cache.get(guilddb.goodbyeChannel);
+	const channel = await member.guild.channels.fetch(guilddb.goodbyeChannel);
 	try {
 		channel.send(`<@${member.id}>, ${guilddb.goodbyeText}`);
 	} catch (error) {

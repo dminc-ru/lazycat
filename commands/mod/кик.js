2ba6d09ed44c28f1d2050c33454adecc92f3dc6a
Dan <whatdanman@gmail.com>
2021-12-23 11:52:46 +0300

    Код команды обновлён до v13

--- commands/mod/кик.js
+++ commands/mod/кик.js
@@ -1,134 +1,54 @@
 const { MessageEmbed } = require("discord.js");
 module.exports.run = async (client, interaction) => {
-try{
-	let user = client.users.cache.get(interaction.member.user.id);
-	let guild = client.guilds.cache.get(interaction.guild_id);
-	let guilddb = await client.db.get(interaction.guild_id, 'guilds')
-	let member = guild.members.cache.get(interaction.member.user.id);
+	let user = await client.users.fetch(interaction.member.user.id);
+	let guild = await client.guilds.fetch(interaction.guild_id);
+	let guilddb = await client.db.getGuild(interaction.guild_id)
+	let member = await guild.members.fetch(interaction.member.user.id);
 	let toKick = interaction.data.options[0].value;
-	let toKickClientResolve = client.users.cache.get(toKick);
-	if(!member.hasPermission('KICK_MEMBERS'))
-	return client.api.interactions(interaction.id, interaction.token).callback.post({
-		data: {
-			type: 4,
-			data: {
-				flags: 64,
-				content: `У вас недостаточно прав для выполнения этой команды.`
-			}
-		}
-	});
-	var toKickResolve = guild.members.cache.get(toKick);
+	let toKickClientResolve = await client.users.fetch(toKick);
+	if ( !member.hasPermission('KICK_MEMBERS') ) {
+		return interaction.reply({content: `У вас недостаточно прав для выполнения этой команды.`, ephemeral: true})
+	}
+	var toKickResolve = await guild.members.fetch(toKick);
 	if(toKickResolve){
 		const memberPosition = toKickResolve.roles.highest.position;
 		const moderationPosition = member.roles.highest.position;
-		if(!(moderationPosition > memberPosition)){
-			return client.api.interactions(interaction.id, interaction.token).callback.post({
-				data: {
-					type: 4,
-					data: {
-						flags: 64,
-						content: `У вас недостаточно прав для выполнения этой команды.`
-					}
-				}
-			});
+		if ( !(moderationPosition > memberPosition) || !toKickResolve.kickable) {
+			return interaction.reply({content: `У вас недостаточно прав для выполнения этой команды.`, ephemeral: true})
 		}
-		if(!toKickResolve.kickable) {
-			return client.api.interactions(interaction.id, interaction.token).callback.post({
-				data: {
-					type: 4,
-					data: {
-						flags: 64,
-						content: `У вас недостаточно прав для выполнения этой команды.`
-					}
-				}
-			});
-		}
-	}else{
-		return client.api.interactions(interaction.id, interaction.token).callback.post({
-			data: {
-				type: 4,
-				data: {
-					flags: 64,
-					content: `Пользователь не найден.`
-				}
-			}
-		});
+	} else {
+		return interaction.reply({content: `Пользователь не найден.`, ephemeral: true})
 	}
-	if(toKick == interaction.member.user.id)
-	return client.api.interactions(interaction.id, interaction.token).callback.post({
-		data: {
-			type: 4,
-			data: {
-				flags: 64,
-				content: `Вы не можете кикнуть себя.`
-			}
-		}
-	});
-	if(toKick == client.user.id)
-	return client.api.interactions(interaction.id, interaction.token).callback.post({
-		data: {
-			type: 4,
-			data: {
-				flags: 64,
-				content: `У вас недостаточно прав для выполнения данного действия.`
-			}
-		}
-	});
-    if(interaction.data.options.length > 1)
+	if (toKick == interaction.member.user.id) {
+		return interaction.reply({content: `Вы не можете кикнуть себя.`, ephemeral: true})
+	}
+	if (toKick == client.user.id) {
+		return interaction.reply({content: `У вас недостаточно прав для выполнения данного действия.`, ephemeral: true})
+	}
+    if (interaction.data.options.length > 1) {
 		var reason = interaction.data.options[1].value
-	else
+	} else {
 		var reason = `не указана`;
-	try{guild.members.kick(toKick, {reason});}catch(error){
+	}
+	try{
+		guild.members.kick(toKick, {reason});
+	} catch(error) {
 		console.log(error)
-		return client.api.interactions(interaction.id, interaction.token).callback.post({
-			data: {
-				type: 4,
-				data: {
-					flags: 64,
-					content: `Произошла ошибка при попытке кика. Возможно, у меня недостаточно прав для выполнения этого действия.`
-				}
-			}
-		});
+		return interaction.reply({content: `Произошла ошибка при попытке кика. Возможно, у меня недостаточно прав для выполнения этого действия.`, ephemeral: true})
 	}
 	let usernames = toKickClientResolve.tag;
-	client.api.interactions(interaction.id, interaction.token).callback.post({
-		data: {
-			type: 4,
-			data: {
-				embeds: [
-					{
-						color: 0xb88fff,
-						title: 'Разбан: успешно',
-						fields: [
-							{
-								name: "Модератор:",
-								value: `${user.tag}`,
-								inline: true
-							},
-							{
-								name: "Пользователь:",
-								value: `${usernames}`,
-								inline: true
-							},
-							{
-								name: "Причина:",
-								value: `${reason}`,
-								inline: false
-							},
-						],
-						timestamp: new Date(),
-						footer: {
-							text: `${user.tag}`,
-							icon_url: `${user.displayAvatarURL()}`,
-						}
-					}
-				]
-			}
-		}
-	});
+	let kickMessage = new MessageEmbed()
+		.setColor(client.config.embedColor)
+		.setTitle('Кик: успешно')
+		.addField('Модератор:', user.tag, true)
+		.addField('Пользователь:', usernames, true)
+		.addField('Причина:', reason, false)
+		.setTimestamp()
+		.setFooter(user.tag, user.displayAvatarURL({dynamic: true}))
+	interaction.reply({embeds: [kickMessage]})
 	if(guilddb.logmsg_channel != ""){
-		try{
-			const channel = guild.channels.cache.get(guilddb.logmsg_channel);
+		try {
+			const channel = await guild.channels.fetch(guilddb.logmsg_channel);
 			let muteMessage = new MessageEmbed()
 			.setColor(`#b88fff`)
 			.setTitle("Кик: успешно")
@@ -136,21 +56,16 @@ try{
 			.addField('Пользователь:', `${usernames}`, true)
 			.addField('Причина:', `${reason}`, false)
 			.setTimestamp()
-			.setFooter(`${user.tag}`, user.displayAvatarURL());
+			.setFooter(`${user.tag}`, user.displayAvatarURL({dynamic: true}));
 			return channel.send(muteMessage);
-		}catch(error){
-			client.db.change(interaction.guild_id, 'guilds', 'logmsg_channel', '');
+		} catch(error) {
+			client.db.changeGuild(interaction.guild_id, 'logmsg_channel', '');
 		}
-	}
-
-}catch(error){
-		client.logger.log(`${error}`, "err");
 	}
 }
 
-module.exports.help = {
+module.exports.data = {
 	name: "кик",
-	aliases: ["rbr"],
 	permissions: ["member"],
-	modules: ["mod"]
+	type: "interaction"
 }

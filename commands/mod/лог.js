3fe687762ac5b6c013bc6a14d765a0086f56a8c9
Dan <whatdanman@gmail.com>
2021-12-23 16:38:18 +0300

    Код команды обновлён до v13

--- commands/mod/лог.js
+++ commands/mod/лог.js
@@ -1,326 +1,133 @@
 module.exports.run = async (client, interaction) => {
-try{
-	let user = client.users.cache.get(interaction.member.user.id);
-	let guild = client.guilds.cache.get(interaction.guild_id);
-	let guilddb = await client.db.get(interaction.guild_id, 'guilds')
-	let member = guild.members.cache.get(interaction.member.user.id);
+	let user = await client.users.fetch(interaction.member.user.id);
+	let guild = await client.guilds.fetch(interaction.guild_id);
+	let guilddb = await client.db.getGuild(interaction.guild_id)
+	let member = await guild.members.fetch(interaction.member.user.id);
 	var typeLog = interaction.data.options[0].name;
-	if(!member.hasPermission('ADMINISTRATOR'))
-	return client.api.interactions(interaction.id, interaction.token).callback.post({
-		data: {
-			type: 4,
-			data: {
-				flags: 64,
-				content: `У вас недостаточно прав для выполнения этой команды.`
-			}
-		}
-	});
-	if(typeLog == "чат"){
-		client.db.change(interaction.guild_id, 'guilds', 'logmsg_channel', interaction.data.options[0].options[1].value)
-		return client.api.interactions(interaction.id, interaction.token).callback.post({
-			data: {
-				type: 4,
-				data: {
-					embeds: [
-						{
-							color: 0xb88fff,
-							title: 'Успешно',
-							description: `Канал для логов установлен. Теперь туда будут отправляться сообщения о действиях модераторов.`,
-							timestamp: new Date(),
-							footer: {
-								text: `${user.tag}`,
-								icon_url: `${user.displayAvatarURL()}`,
-							}
-						}
-					]
-				}
-			}
-		});
+	if ( !member.hasPermission('ADMINISTRATOR') ) {
+		return interaction.reply({content: `У вас недостаточно прав для выполнения этой команды.`, ephemeral: true})
 	}
-	if(typeLog == "мсг"){
+	if (typeLog == "чат") {
+		client.db.changeGuild(interaction.guild_id, 'logmsg_channel', interaction.data.options[0].options[1].value)
+		let successEmbed = new MessageEmbed()
+			.setColor(client.config.embedColor)
+			.setTitle('Успешно')
+			.setDescription('Канал для логов установлен. Теперь туда будут отправляться сообщения о действиях модераторов.')
+			.setTimestamp()
+			.setFooter(user.tag, user.displayAvatarURL({dynamic: true}))
+		return interaction.reply({embeds: [successEmbed]})
+	}
+	if (typeLog == "мсг") {
 		let typeAction = interaction.data.options[0].options[0].name;
-		if(typeAction == "вкл"){
-			if(guilddb.logmsg_enable == 'false'){
-				return client.api.interactions(interaction.id, interaction.token).callback.post({
-					data: {
-						type: 4,
-						data: {
-							flags: 64,
-							content: `Аудит сообщений уже включен. Включить — /лог мсг выкл`
-						}
-					}
-				});
+		if (typeAction == "вкл"){
+			if (guilddb.logmsg_enable == 'false') {
+				return interaction.reply({content: `Аудит сообщений уже включен. Включить — /лог мсг выкл`, ephemeral: true})
 			}
-			if(guilddb.logmsg_channel == ''){
-				return client.api.interactions(interaction.id, interaction.token).callback.post({
-					data: {
-						type: 4,
-						data: {
-							flags: 64,
-							content: `Сначала необходимо установить канал для логов — /лог чат <#канал>`
-						}
-					}
-				});
+			if (guilddb.logmsg_channel == '') {
+				return interaction.reply({content: `Сначала необходимо установить канал для логов — /лог чат <#канал>`, ephemeral: true})
 			}
-			client.db.change(interaction.guild_id, 'guilds', 'logmsg_enable', 'true')
-			client.db.change(interaction.guild_id, 'guilds', 'logmsg_type_edit', 'false')
-			client.db.change(interaction.guild_id, 'guilds', 'logmsg_type_delete', 'true')
-			return client.api.interactions(interaction.id, interaction.token).callback.post({
-				data: {
-					type: 4,
-					data: {
-						embeds: [
-							{
-								color: 0xb88fff,
-								title: 'Успешно',
-								description: `Аудит сообщений включён. Отключить — /лог мсг выкл`,
-								timestamp: new Date(),
-								footer: {
-									text: `${user.tag}`,
-									icon_url: `${user.displayAvatarURL()}`,
-								}
-							}
-						]
-					}
-				}
-			});
+			client.db.changeGuild(interaction.guild_id, 'logmsg_enable', 'true')
+			client.db.changeGuild(interaction.guild_id, 'logmsg_type_edit', 'false')
+			client.db.changeGuild(interaction.guild_id, 'logmsg_type_delete', 'true')
+			let auditEnabled = new MessageEmbed()
+				.setColor(client.config.embedColor)
+				.setTitle('Успешно')
+				.setDescription(`Аудит сообщений включён. Отключить — /лог мсг выкл`)
+				.setTimestamp()
+				.setFooter(user.tag, user.displayAvatarURL({dynamic: true}))
+			return interaction.reply({embeds: [auditEnabled]})
 		}
-		if(typeAction == "выкл"){
-			if(guilddb.logmsg_enable == 'false'){
-				return client.api.interactions(interaction.id, interaction.token).callback.post({
-					data: {
-						type: 4,
-						data: {
-							flags: 64,
-							content: `Аудит сообщений уже выключен. Включить — /лог мсг вкл`
-						}
-					}
-				});
+		if (typeAction == "выкл"){
+			if (guilddb.logmsg_enable == 'false'){
+				return interaction.reply({content: `Аудит сообщений уже выключен. Включить — /лог мсг вкл`, ephemeral: true})
 			}
-			if(guilddb.logmsg_channel == ''){
-				return client.api.interactions(interaction.id, interaction.token).callback.post({
-					data: {
-						type: 4,
-						data: {
-							flags: 64,
-							content: `Сначала необходимо установить канал для логов — /лог чат <#канал>`
-						}
-					}
-				});
+			if (guilddb.logmsg_channel == ''){
+				return interaction.reply({content: `Сначала необходимо установить канал для логов — /лог чат <#канал>`, ephemeral: true})
 			}
-			client.db.change(interaction.guild_id, 'guilds', 'logmsg_enable', 'false')
-			return client.api.interactions(interaction.id, interaction.token).callback.post({
-				data: {
-					type: 4,
-					data: {
-						embeds: [
-							{
-								color: 0xb88fff,
-								title: 'Успешно',
-								description: `Аудит сообщений выключен. Включить снова — /лог мсг вкл`,
-								timestamp: new Date(),
-								footer: {
-									text: `${user.tag}`,
-									icon_url: `${user.displayAvatarURL()}`,
-								}
-							}
-						]
-					}
-				}
-			});
+			client.db.changeGuild(interaction.guild_id, 'logmsg_enable', 'false')
+			let auditDisabled = new MessageEmbed()
+				.setColor(client.config.embedColor)
+				.setTitle('Успешно')
+				.setDescription(`Аудит сообщений выключен. Включить снова — /лог мсг вкл`)
+				.setTimestamp()
+				.setFooter(user.tag, user.displayAvatarURL({dynamic: true}))
+			return interaction.reply({embeds: [auditDisabled]})
 		}
-		if(typeAction == "удалить"){
+		if (typeAction == "удалить"){
 			let subTypeAction = interaction.data.options[0].options[0].options[0].name;
 			if(guilddb.logmsg_enable == 'false'){
-				return client.api.interactions(interaction.id, interaction.token).callback.post({
-					data: {
-						type: 4,
-						data: {
-							flags: 64,
-							content: `Сначала нужно включить аудит сообщений — /логмсг вкл`
-						}
-					}
-				});
+				return interaction.reply({content: `Сначала нужно включить аудит сообщений — /логмсг вкл`, ephemeral: true})
 			}
 			if(guilddb.logmsg_channel == ''){
-				return client.api.interactions(interaction.id, interaction.token).callback.post({
-					data: {
-						type: 4,
-						data: {
-							flags: 64,
-							content: `Сначала необходимо установить канал для логов — /лог чат <#канал>`
-						}
-					}
-				});
+				return interaction.reply({content: `Сначала необходимо установить канал для логов — /лог чат <#канал>`, ephemeral: true})
 			}
 			if(subTypeAction == "вкл"){
 				if(guilddb.logmsg_type_delete == 'true'){
-					return client.api.interactions(interaction.id, interaction.token).callback.post({
-						data: {
-							type: 4,
-							data: {
-								flags: 64,
-								content: `Аудит удаления сообщений уже включен. Выключить — /лог мсг удалить выкл`
-							}
-						}
-					});
+					return interaction.reply({content: `Аудит удаления сообщений уже включен. Выключить — /лог мсг удалить выкл`, ephemeral: true})
 				}
-				client.db.change(interaction.guild_id, 'guilds', 'logmsg_type_delete', 'true')
-				return client.api.interactions(interaction.id, interaction.token).callback.post({
-					data: {
-						type: 4,
-						data: {
-							embeds: [
-								{
-									color: 0xb88fff,
-									title: 'Успешно',
-									description: `Аудит удаления сообщений включен. Выключить — /лог мсг удалить выкл`,
-									timestamp: new Date(),
-									footer: {
-										text: `${user.tag}`,
-										icon_url: `${user.displayAvatarURL()}`,
-									}
-								}
-							]
-						}
-					}
-				});
+				client.db.changeGuild(interaction.guild_id, 'logmsg_type_delete', 'true')
+				let auditDeleteEnabled = new MessageEmbed()
+					.setColor(client.config.embedColor)
+					.setTitle('Успешно')
+					.setDescription(`Аудит удаления сообщений включен. Выключить — /лог мсг удалить выкл`)
+					.setTimestamp()
+					.setFooter(user.tag, user.displayAvatarURL({dynamic: true}))
+				return interaction.reply({embeds: [auditDeleteEnabled]})
 			}
 			if(subTypeAction == "выкл"){
 				if(guilddb.logmsg_type_delete == 'false'){
-					return client.api.interactions(interaction.id, interaction.token).callback.post({
-						data: {
-							type: 4,
-							data: {
-								flags: 64,
-								content: `Аудит удаления сообщений уже выключен. Включить — /лог мсг удалить вкл`
-							}
-						}
-					});
+					return interaction.reply({content: `Аудит удаления сообщений уже выключен. Включить — /лог мсг удалить вкл`, ephemeral: true})
 				}
-				client.db.change(interaction.guild_id, 'guilds', 'logmsg_type_delete', 'false')
-				return client.api.interactions(interaction.id, interaction.token).callback.post({
-					data: {
-						type: 4,
-						data: {
-							embeds: [
-								{
-									color: 0xb88fff,
-									title: 'Успешно',
-									description: `Аудит удаления сообщений выключен. Включить — /лог мсг удалить вкл`,
-									timestamp: new Date(),
-									footer: {
-										text: `${user.tag}`,
-										icon_url: `${user.displayAvatarURL()}`,
-									}
-								}
-							]
-						}
-					}
-				});
+				client.db.changeGuild(interaction.guild_id, 'logmsg_type_delete', 'false')
+				let auditDeleteDisabled = new MessageEmbed()
+					.setColor(client.config.embedColor)
+					.setTitle('Успешно')
+					.setDescription('Аудит удаления сообщений выключен. Включить — /лог мсг удалить вкл')
+					.setTimestamp()
+					.setFooter(user.tag, user.displayAvatarURL({dynamic: true}))
+				return interaction.reply({embeds: [auditDeleteDisabled]})
 			}
 		}
 		if(typeAction == "эдит"){
 			let subTypeAction = interaction.data.options[0].options[0].options[0].name;
 			if(guilddb.logmsg_enable == 'false'){
-				return client.api.interactions(interaction.id, interaction.token).callback.post({
-					data: {
-						type: 4,
-						data: {
-							flags: 64,
-							content: `Сначала нужно включить аудит сообщений — /лог мсг вкл`
-						}
-					}
-				});
+				return interaction.reply({content: `Сначала нужно включить аудит сообщений — /лог мсг вкл`, ephemeral: true})
 			}
 			if(guilddb.logmsg_channel == ''){
-				return client.api.interactions(interaction.id, interaction.token).callback.post({
-					data: {
-						type: 4,
-						data: {
-							flags: 64,
-							content: `Сначала необходимо установить канал для логов — /лог чат <#канал>`
-						}
-					}
-				});
+				return interaction.reply({content: `Сначала необходимо установить канал для логов — /лог чат <#канал>`, ephemeral: true})
 			}
 			if(subTypeAction == "вкл"){
 				if(guilddb.logmsg_type_edit == 'true'){
-					return client.api.interactions(interaction.id, interaction.token).callback.post({
-						data: {
-							type: 4,
-							data: {
-								flags: 64,
-								content: `Аудит редактирования сообщений уже включен. Выключить — /лог мсг эдит выкл`
-							}
-						}
-					});
+					return interaction.reply({content: `Аудит редактирования сообщений уже включен. Выключить — /лог мсг эдит выкл`, ephemeral: true})
 				}
-				client.db.change(interaction.guild_id, 'guilds', 'logmsg_type_edit', 'true')
-				return client.api.interactions(interaction.id, interaction.token).callback.post({
-					data: {
-						type: 4,
-						data: {
-							embeds: [
-								{
-									color: 0xb88fff,
-									title: 'Успешно',
-									description: `Аудит редактирования сообщений включен. Выключить — /лог мсг эдит выкл`,
-									timestamp: new Date(),
-									footer: {
-										text: `${user.tag}`,
-										icon_url: `${user.displayAvatarURL()}`,
-									}
-								}
-							]
-						}
-					}
-				});
+				client.db.changeGuild(interaction.guild_id, 'logmsg_type_edit', 'true')
+				let auditEditEnabled = new MessageEmbed()
+					.setColor(client.config.embedColor)
+					.setTitle('Успешно')
+					.setDescription(`Аудит редактирования сообщений включен. Выключить — /лог мсг эдит выкл`)
+					.setTimestamp()
+					.setFooter(user.tag, user.displayAvatarURL({dynamic: true}))
+				return interaction.reply({embeds: [auditEditEnabled]})
 			}
 			if(subTypeAction == "выкл"){
 				if(guilddb.logmsg_type_edit == 'false'){
-					return client.api.interactions(interaction.id, interaction.token).callback.post({
-						data: {
-							type: 4,
-							data: {
-								flags: 64,
-								content: `Аудит редактирования сообщений уже выключен. Включить — /лог мсг эдит вкл`
-							}
-						}
-					});
+					return interaction.reply({content: `Аудит редактирования сообщений уже выключен. Включить — /лог мсг эдит вкл`, ephemeral: true})
 				}
-				client.db.change(interaction.guild_id, 'guilds', 'logmsg_type_edit', 'false')
-				return client.api.interactions(interaction.id, interaction.token).callback.post({
-					data: {
-						type: 4,
-						data: {
-							embeds: [
-								{
-									color: 0xb88fff,
-									title: 'Успешно',
-									description: `Аудит редактирования сообщений выключен. Включить — /лог мсг эдит вкл`,
-									timestamp: new Date(),
-									footer: {
-										text: `${user.tag}`,
-										icon_url: `${user.displayAvatarURL()}`,
-									}
-								}
-							]
-						}
-					}
-				});
+				client.db.changeGuild(interaction.guild_id, 'logmsg_type_edit', 'false')
+				let auditEditDisabled = new MessageEmbed()
+					.setColor(client.config.embedColor)
+					.setTitle('Успешно')
+					.setDescription('Аудит редактирования сообщений выключен. Включить — /лог мсг эдит вкл')
+					.setTimestamp()
+					.setFooter(user.tag, user.displayAvatarURL({dynamic: true}))
+				return interaction.reply({embeds: [auditEditDisabled]})
 			}
 		}
 	}
-
-}catch(error){
-		client.logger.log(`${error}`, "err");
-	}
 }
 
-module.exports.help = {
+module.exports.data = {
 	name: "лог",
-	aliases: ["kjuxfn"],
 	permissions: ["tester"],
-	modules: ["tech"]
+	type: "interaction"
 }

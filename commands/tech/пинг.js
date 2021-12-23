523f11fe62f3e1e25bb80a6e45b622fd33fb12ce
Dan <whatdanman@gmail.com>
2021-12-23 18:06:39 +0300

    Код команды обновлён до v13

--- commands/tech/пинг.js
+++ commands/tech/пинг.js
@@ -1,41 +1,21 @@
 const ms = require("ms");
 module.exports.run = async (client, interaction) => {
-	try{
-		let user = client.users.cache.get(interaction.member.user.id);
-		var members = client.guilds.cache.reduce((a, g) => a + g.memberCount, 0)
-		client.api.interactions(interaction.id, interaction.token).callback.post({
-			data: {
-				type: 4,
-				data: {
-				embeds: [
-						{
-							color: 0xb88fff,
-							title: 'Понг!',
-							description: `Задержка: **${Math.round(client.ws.ping)}**мс\n
-							<:lz_cloud:774302583564402748> Серверов: ${client.guilds.cache.size}.
-							<:lz_users:774302509333348372> Пользователей: ${members}.
-							<:lz_time:774532168138489856> Аптайм: ${ms(client.uptime, {long: true})}\n
-							<:lz_music:817396644471111690> Радио: работает в ${client.voice.connections.size} серв.`,
-							timestamp: new Date(),
-							footer: {
-								text: `${user.tag}`,
-								icon_url: `${user.displayAvatarURL()}`,
-							}
-						}
-					]
-				}
-			}
-		});
-	}
-	catch(error){
-		client.logger.log(`${error}`, "err");
-		console.log(error)
-	}
+	let user = await client.users.fetch(interaction.member.user.id);
+	var members = client.guilds.cache.reduce((a, g) => a + g.memberCount, 0)
+	let pongEmbed = new MessageEmbed()
+		.setTitle('Понг!')
+		.setDescription(`Задержка: **${Math.round(client.ws.ping)}**мс\n
+			${client.emoji.cloud} Серверов: ${client.guilds.cache.size}.
+			${client.emoji.users} Пользователей: ${members}.
+			${client.emoji.time} Аптайм: ${ms(client.uptime, {long: true})}\n
+			${client.emoji.music} Радио: работает в ${client.voice.connections.size} серв.`)
+		.setTimestamp()
+		.setFooter(user.tag, user.displayAvatarURL({dynamic: true}))
+	interaction.reply({embeds: [pongEmbed]})
 }
 
-module.exports.help = {
+module.exports.data = {
 	name: "пинг",
-	aliases: ["gbyu"],
 	permissions: ["member"],
-	modules: ["tech"]
+	type: "interaction"
 }

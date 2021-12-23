9d0e9d1236320be5f31f54756fec38821efcdd34
Dan <whatdanman@gmail.com>
2021-12-23 18:23:36 +0300

    Код обновлён

--- events/messageDelete.js
+++ events/messageDelete.js
@@ -1,25 +1,25 @@
-let messages = require(`${process.env.PATHTOBASE}/messages.json`);
+let messages = require(`${client.config.jsonPath}messages.json`);
 let fs = require("fs");
 module.exports = async (client, message, channel) => {
-	let guilddb = await client.db.get(message.guild.id, 'guilds')
-    if(message.author.id == client.user.id)
+	let guilddb = await client.db.getGuild(message.guild.id)
+    if (message.author.id == client.user.id)
 		return;
-	if(!messages[message.guild.id]){
+	if (!messages[message.guild.id]) {
         messages[message.guild.id] = [];
     }
-	fs.writeFileSync(`${process.env.PATHTOBASE}\\messages.json`, JSON.stringify(messages, null, "\t"));
+	fs.writeFileSync(`${client.config.jsonPath}messages.json`, JSON.stringify(messages, null, "\t"));
 	let messageExists = messages[message.guild.id].findIndex(x => x.msgid === message.id);
-	if(messageExists != -1){
+	if (messageExists != -1) {
 		messages[message.guild.id].splice(messageExists, 1);
-		fs.writeFileSync(`${process.env.PATHTOBASE}\\messages.json`, JSON.stringify(messages, null, "\t"));
+		fs.writeFileSync(`${client.config.jsonPath}messages.json`, JSON.stringify(messages, null, "\t"));
 	}
-	if(guilddb.logmsg_channel == "")
+	if (guilddb.logmsg_channel == "")
 		return;
-	if(guilddb.logmsg_enable == 'false')
+	if (guilddb.logmsg_enable == 'false')
 		return;
-	if(guilddb.logmsg_type_delete == 'false')
+	if (guilddb.logmsg_type_delete == 'false')
 		return;
-	if(message.length > 500)
+	if (message.length > 500)
 		return;
 	let embed = new MessageEmbed()
 		.setColor("b88fff")
@@ -28,7 +28,7 @@ module.exports = async (client, message, channel) => {
 		.addField(`Канал:`, `${message.channel.name}`, true)
 		.addField(`Автор:`, `${message.author.tag}`, true)
 		.setTimestamp()
-		.setFooter(`Lazy Cat`, client.user.avatarURL());
-	let logChan = message.guild.channels.cache.get(guilddb.logmsg_channel);
+		.setFooter(`Lazy Cat`, client.user.displayAvatarURL({dynamic: true}));
+	let logChan = await message.guild.channels.fetch(guilddb.logmsg_channel);
 	logChan.send(embed);
 }

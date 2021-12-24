bce1b65c38366070756bfd93a79ed13bfd156562
Dan <whatdanman@gmail.com>
2021-12-24 23:00:31 +0300

    Добавление вывода ошибок

--- commands/admin/lzuser.js
+++ commands/admin/lzuser.js
@@ -1,45 +1,50 @@
 const moment = require("moment")
 const { MessageEmbed } = require('discord.js')
 module.exports.run = async (client, message, args) => {
-    const stats = require(`${client.config.jsonPath}stats.json`)
-    const works = require(`${client.config.jsonPath}works.json`)
-    let noInfo = new MessageEmbed()
-		.setColor(client.config.embedColor)
-		.setTitle(`Ошибка`)
-		.setDescription(`Пользователь не найден.`)
-        .setTimestamp()
-        .setFooter(`${stats.version}`, client.user.avatarURL()) 
-	if (!args[0]) {
-        return message.channel.send({embeds: [noInfo]})
-    }
     try{
-        var user = await client.users.fetch((message.mentions.users.first() || args[0]));
+        const stats = require(`${client.config.jsonPath}stats.json`)
+        const works = require(`${client.config.jsonPath}works.json`)
+        let noInfo = new MessageEmbed()
+            .setColor(client.config.embedColor)
+            .setTitle(`Ошибка`)
+            .setDescription(`Пользователь не найден.`)
+            .setTimestamp()
+            .setFooter(`${stats.version}`, client.user.avatarURL()) 
+        if (!args[0]) {
+            return message.channel.send({embeds: [noInfo]})
+        }
+        try{
+            var user = await client.users.fetch((message.mentions.users.first() || args[0]));
+        } catch (error) {
+            return message.channel.send({embeds: [noInfo]})
+        }
+        let userdb = await client.db.getUser(user.id);
+        if (!userdb) {
+            return message.channel.send({embeds: [noInfo]});
+        }
+        var work = works.find(wrk => wrk.codename === userdb.work_current)
+        let userInfo = new MessageEmbed()
+            .setColor(client.config.embedColor)
+            .setAuthor(user.tag, user.displayAvatarURL({dynamic: true}))
+            .setTitle(`Информация о пользователе`)
+            .addField(`Рыбок:`, `\`${userdb.balance_fish}\` ${client.emoji.fish}`, true)
+            .addField(`Жучков:`, `\`${userdb.balance_bugs}\` ${client.emoji.bug}`, true)
+            .addField(`Описание:`, `\`${userdb.description}\``, true)
+            .addField(`Присоединился:`, `\`${moment(new Date(Number(userdb.join_date))).locale('ru').format('LLL')}\``, false)
+            .addField(`Ежедневная награда:`, `\`${(Number(userdb.reward_daily) > Date.now()) ? moment(new Date(Number(userdb.reward_daily))).locale('ru').fromNow() : `доступна`}\``, true)
+            .addField(`Еженедельная награда:`, `\`${(Number(userdb.reward_weekly) > Date.now()) ? moment(new Date(Number(userdb.reward_weekly))).locale('ru').fromNow() : `доступна`}\``, true)
+            .addField(`Математика:`, `\`${userdb.math_level}\``, true)
+            .addField(`Премиум:`, `\`${(userdb.premium_has == 0) ? `не активен` : `активен`}\``, true)
+            .addField(`Действует до:`, `\`${(userdb.premium_until == '') ? `-` : moment(new Date(Number(userdb.premium_until))).locale('ru').endOf('day').fromNow()}\``, true)
+            .addField(`Работа:`, `\`${(userdb.work_current == '') ? `не работает` : work.name}\``, false)
+            .addField(`Текущий ранг:`, `\`${work.ranks[userdb.work_rank].name} (${userdb.work_rank}/${work.ranks.length})\``, true)
+            .addField(`Количество XP:`, `\`${userdb.work_currentXP}/${work.ranks[userdb.work_rank + 1].requiredXP}\``, true)
+            .setFooter(message.author.tag, message.author.displayAvatarURL({ dynamic: true }))
+            message.channel.send({embeds: [userInfo]});
     } catch (error) {
-        return message.channel.send({embeds: [noInfo]})
+        client.logger.log(error, 'err')
+        console.error(error)
     }
-    let userdb = await client.db.getUser(user.id);
-    if (!userdb) {
-        return message.channel.send({embeds: [noInfo]});
-    }
-    var work = works.find(wrk => wrk.codename === userdb.work_current)
-    let userInfo = new MessageEmbed()
-		.setColor(client.config.embedColor)
-        .setAuthor(user.tag, user.displayAvatarURL({dynamic: true}))
-		.setTitle(`Информация о пользователе`)
-		.addField(`Рыбок:`, `\`${userdb.balance_fish}\` ${client.emoji.fish}`, true)
-        .addField(`Жучков:`, `\`${userdb.balance_bugs}\` ${client.emoji.bug}`, true)
-        .addField(`Описание:`, `\`${userdb.description}\``, true)
-        .addField(`Присоединился:`, `\`${moment(new Date(Number(userdb.join_date))).locale('ru').format('LLL')}\``, false)
-        .addField(`Ежедневная награда:`, `\`${(Number(userdb.reward_daily) > Date.now()) ? moment(new Date(Number(userdb.reward_daily))).locale('ru').fromNow() : `доступна`}\``, true)
-        .addField(`Еженедельная награда:`, `\`${(Number(userdb.reward_weekly) > Date.now()) ? moment(new Date(Number(userdb.reward_weekly))).locale('ru').fromNow() : `доступна`}\``, true)
-        .addField(`Математика:`, `\`${userdb.math_level}\``, true)
-		.addField(`Премиум:`, `\`${(userdb.premium_has == 0) ? `не активен` : `активен`}\``, true)
-        .addField(`Действует до:`, `\`${(userdb.premium_until == '') ? `-` : moment(new Date(Number(userdb.premium_until))).locale('ru').endOf('day').fromNow()}\``, true)
-        .addField(`Работа:`, `\`${(userdb.work_current == '') ? `не работает` : work.name}\``, false)
-        .addField(`Текущий ранг:`, `\`${work.ranks[userdb.work_rank].name} (${userdb.work_rank}/${work.ranks.length})\``, true)
-        .addField(`Количество XP:`, `\`${userdb.work_currentXP}/${work.ranks[userdb.work_rank + 1].requiredXP}\``, true)
-        .setFooter(message.author.tag, message.author.displayAvatarURL({ dynamic: true }))
-        message.channel.send({embeds: [userInfo]});
 }
 
 module.exports.data = {
--- commands/cases/инвентарь.js
+++ commands/cases/инвентарь.js
@@ -1,106 +1,111 @@
 const fs = require("fs");
 const { MessageEmbed } = require('discord.js')
 module.exports.run = async (client, interaction) => {
-	let inventory = require(`${client.config.jsonPath}inventory.json`);
-	let user = await client.users.fetch(interaction.member.user.id);
-	if(!inventory[interaction.member.user.id]){
-		inventory[interaction.member.user.id] = {
-			cases: [
-				{
-					caseID: "1",
-					count: 0,
-					megaCount: 0,
-					luckyCount: 0
-				},
-				{
-					caseID: "2",
-					count: 0,
-					megaCount: 0,
-					luckyCount: 0
-				}
-			],
-			items: []
-		};
-		fs.writeFileSync(`${client.config.jsonPath}inventory.json`, JSON.stringify(inventory, null, "\t"));
-	}
-	let comlength = Object.keys(inventory[interaction.member.user.id].items).length;
-	if(comlength == 0){
-		let inventorys = "Пусто. Откройте кейс!";
-		let inventoryEmbed = new MessageEmbed()
-			.setColor(client.config.embedColor)
-			.setTitle(`Инвентарь ${user.tag}`)
-			.setDescription(inventorys)
-			.setTimestamp()
-			.setFooter(user.tag, user.displayAvatarURL({dynamic: true}))
-		return interaction.reply({embeds: [inventoryEmbed]});
-	}
-	let number = 1;
-	pages = Math.ceil(comlength/15);
-	if(!interaction.data.options){
+	try{
+		let inventory = require(`${client.config.jsonPath}inventory.json`);
+		let user = await client.users.fetch(interaction.member.user.id);
+		if(!inventory[interaction.member.user.id]){
+			inventory[interaction.member.user.id] = {
+				cases: [
+					{
+						caseID: "1",
+						count: 0,
+						megaCount: 0,
+						luckyCount: 0
+					},
+					{
+						caseID: "2",
+						count: 0,
+						megaCount: 0,
+						luckyCount: 0
+					}
+				],
+				items: []
+			};
+			fs.writeFileSync(`${client.config.jsonPath}inventory.json`, JSON.stringify(inventory, null, "\t"));
+		}
+		let comlength = Object.keys(inventory[interaction.member.user.id].items).length;
+		if(comlength == 0){
+			let inventorys = "Пусто. Откройте кейс!";
+			let inventoryEmbed = new MessageEmbed()
+				.setColor(client.config.embedColor)
+				.setTitle(`Инвентарь ${user.tag}`)
+				.setDescription(inventorys)
+				.setTimestamp()
+				.setFooter(user.tag, user.displayAvatarURL({dynamic: true}))
+			return interaction.reply({embeds: [inventoryEmbed]});
+		}
+		let number = 1;
+		pages = Math.ceil(comlength/15);
+		if(!interaction.data.options){
+			let inventoryss = "";
+			for(i=0; i<15; i++){
+			if(inventory[interaction.member.user.id].items[i]){
+				inventoryss += `${number}. ${inventory[interaction.member.user.id].items[i].itemname} • ${inventory[interaction.member.user.id].items[i].cost} ${inventory[interaction.member.user.id].items[i].currency} • ${inventory[interaction.member.user.id].items[i].county} шт. • ${inventory[interaction.member.user.id].items[i].heart}\n`;
+				number+=1;
+			}
+			if(inventoryss == "")
+				inventorys = "Пусто. Откройте кейс!";
+			}
+		
+			if(inventoryss == "")
+				inventorys = "Пусто. Откройте кейс!";
+				let inventoryEmbed = new MessageEmbed()
+					.setColor(client.config.embedColor)
+					.setTitle(`Инвентарь ${user.tag}`)
+					.setDescription(inventoryss)
+					.setTimestamp()
+					.setFooter(`${user.tag} • Страница 1/${pages}`, user.displayAvatarURL({dynamic: true}))
+				return interaction.reply({embeds: [inventoryEmbed]})
+		}
+		let page = interaction.data.options[0].value;
+		if ( (!Number(page)) || (page <= 0) || (page > pages) ){
+			return interaction.reply({content: `Некорректная страница.`, ephemeral: true})
+		}
 		let inventoryss = "";
-		for(i=0; i<15; i++){
-		if(inventory[interaction.member.user.id].items[i]){
-			inventoryss += `${number}. ${inventory[interaction.member.user.id].items[i].itemname} • ${inventory[interaction.member.user.id].items[i].cost} ${inventory[interaction.member.user.id].items[i].currency} • ${inventory[interaction.member.user.id].items[i].county} шт. • ${inventory[interaction.member.user.id].items[i].heart}\n`;
-			number+=1;
-		}
-		if(inventoryss == "")
-			inventorys = "Пусто. Откройте кейс!";
-		}
-	
-		if(inventoryss == "")
-			inventorys = "Пусто. Откройте кейс!";
-			let inventoryEmbed = new MessageEmbed()
-				.setColor(client.config.embedColor)
-				.setTitle(`Инвентарь ${user.tag}`)
-				.setDescription(inventoryss)
-				.setTimestamp()
-				.setFooter(`${user.tag} • Страница 1/${pages}`, user.displayAvatarURL({dynamic: true}))
-			return interaction.reply({embeds: [inventoryEmbed]})
-	}
-	let page = interaction.data.options[0].value;
-	if ( (!Number(page)) || (page <= 0) || (page > pages) ){
-		return interaction.reply({content: `Некорректная страница.`, ephemeral: true})
-	}
-	let inventoryss = "";
-	if(page){
-		if(page > 1){
-			var prpage = page - 1;
-			var i = 15*prpage;
-			number = i + 1;
-			var ogr = page * 15;
-			for(i; i<ogr; i++){
-				if(inventory[interaction.member.user.id].items[i]){
-					inventoryss += `${number}. ${inventory[interaction.member.user.id].items[i].itemname} • ${inventory[interaction.member.user.id].items[i].cost} ${inventory[interaction.member.user.id].items[i].currency} • ${inventory[interaction.member.user.id].items[i].county} шт. • ${inventory[interaction.member.user.id].items[i].heart}\n`;
-					number+=1;
+		if(page){
+			if(page > 1){
+				var prpage = page - 1;
+				var i = 15*prpage;
+				number = i + 1;
+				var ogr = page * 15;
+				for(i; i<ogr; i++){
+					if(inventory[interaction.member.user.id].items[i]){
+						inventoryss += `${number}. ${inventory[interaction.member.user.id].items[i].itemname} • ${inventory[interaction.member.user.id].items[i].cost} ${inventory[interaction.member.user.id].items[i].currency} • ${inventory[interaction.member.user.id].items[i].county} шт. • ${inventory[interaction.member.user.id].items[i].heart}\n`;
+						number+=1;
+					}
 				}
-			}
-			let inventoryEmbed = new MessageEmbed()
-				.setColor(client.config.embedColor)
-				.setTitle(`Инвентарь ${user.tag}`)
-				.setDescription(inventoryss)
-				.setTimestamp()
-				.setFooter(`${user.tag} • Страница ${page}/${pages}`, user.displayAvatarURL({dynamic: true}))
-			return interaction.reply({embeds: [inventoryEmbed]})
-			}
-			i = 0;
-			number = 1;
-			var ogr = page * 15;
-		}
-		if(page == 1){
-			for(i; i<15; i++){
-				if(inventory[interaction.member.user.id].items[i]){
-					inventoryss += `${number}. ${inventory[interaction.member.user.id].items[i].itemname} • ${inventory[interaction.member.user.id].items[i].cost} ${inventory[interaction.member.user.id].items[i].currency} • ${inventory[interaction.member.user.id].items[i].county} шт. • ${inventory[interaction.member.user.id].items[i].heart}\n`;
-					number+=1;
+				let inventoryEmbed = new MessageEmbed()
+					.setColor(client.config.embedColor)
+					.setTitle(`Инвентарь ${user.tag}`)
+					.setDescription(inventoryss)
+					.setTimestamp()
+					.setFooter(`${user.tag} • Страница ${page}/${pages}`, user.displayAvatarURL({dynamic: true}))
+				return interaction.reply({embeds: [inventoryEmbed]})
 				}
+				i = 0;
+				number = 1;
+				var ogr = page * 15;
 			}
-			let inventoryEmbed = new MessageEmbed()
-				.setColor(client.config.embedColor)
-				.setTitle(`Инвентарь ${user.tag}`)
-				.setDescription(inventoryss)
-				.setTimestamp()
-				.setFooter(`${user.tag} • Страница ${page}/${pages}`, user.displayAvatarURL({dynamic: true}))
-			return interaction.reply({embeds: [inventoryEmbed]})
-		}
+			if(page == 1){
+				for(i; i<15; i++){
+					if(inventory[interaction.member.user.id].items[i]){
+						inventoryss += `${number}. ${inventory[interaction.member.user.id].items[i].itemname} • ${inventory[interaction.member.user.id].items[i].cost} ${inventory[interaction.member.user.id].items[i].currency} • ${inventory[interaction.member.user.id].items[i].county} шт. • ${inventory[interaction.member.user.id].items[i].heart}\n`;
+						number+=1;
+					}
+				}
+				let inventoryEmbed = new MessageEmbed()
+					.setColor(client.config.embedColor)
+					.setTitle(`Инвентарь ${user.tag}`)
+					.setDescription(inventoryss)
+					.setTimestamp()
+					.setFooter(`${user.tag} • Страница ${page}/${pages}`, user.displayAvatarURL({dynamic: true}))
+				return interaction.reply({embeds: [inventoryEmbed]})
+			}
+	} catch(error) {
+		client.logger.log(error, 'err')
+		console.error(error)
+	}
 }
 
 module.exports.data = {

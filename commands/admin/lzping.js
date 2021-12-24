b6d7f882bb3942eee7236649d1abb28870e52e40
Dan <whatdanman@gmail.com>
2021-12-24 22:44:23 +0300

    Очистка кода

--- commands/admin/lzping.js
+++ commands/admin/lzping.js
@@ -3,14 +3,15 @@ const ms = require("ms");
 const os = require('os');
 module.exports.run = async (client, message, args) => {
 	let stats = require(`${client.config.jsonPath}stats.json`);
-	var busymem = os.totalmem() - os.freemem()
+	var busymem = (((os.totalmem()/(1024*1024*1024)).toFixed(2)) - ((os.freemem()/(1024*1024*1024).toFixed(2))))
 	let pongEmbed = new MessageEmbed()
 		.setColor(client.config.embedColor)
 		.setTitle(`Lazy Cat: server information`)
-		.addField(`Пинг:`, `\`\`\`${Math.round(client.ws.ping)}ms\`\`\``, true)
-		.addField(`Серверов:`, `\`\`\`${client.guilds.cache.size}\`\`\``, true)
+		.addField(`Пинг:`, `\`\`\`${Math.round(client.ws.ping)}мс\`\`\``, true)
+		.addField(`Серверов в кэше:`, `\`\`\`${client.guilds.cache.size}\`\`\``, true)
+		.addField(`Пользователей в кэше:`, `\`\`\`${client.users.cache.size}\`\`\``, true)
 		.addField(`Аптайм:`, `\`\`\`${ms(client.uptime)}\`\`\``, true)
-		.addField(`RAM:`, `\`\`\`${(busymem/ (1024*1024*1024) ).toFixed(1)}GB/${(os.totalmem()/(1024*1024*1024)).toFixed(1)}GB\`\`\``, true)
+		.addField(`RAM:`, `\`\`\`${busymem.toFixed(2)}GB/${(os.totalmem()/(1024*1024*1024)).toFixed(2)}GB\`\`\``, true)
 		.addField(`Скорость CPU`, `\`\`\`${(os.cpus()[0].speed / 1000).toFixed(2)} GHz\`\`\``, true)
 		.addField(`Модель CPU:`, `\`\`\`${os.cpus()[0].model}\`\`\``, false)
 		.setTimestamp()

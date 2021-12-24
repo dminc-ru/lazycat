76a5ebe3372e66d4cd51559fa0bf941aba995843
Dan <whatdanman@gmail.com>
2021-12-24 22:16:11 +0300

    Очистка кода

--- commands/admin/lzbdg.js
+++ commands/admin/lzbdg.js
@@ -2,7 +2,7 @@ const { MessageEmbed } = require("discord.js");
 const fs = require("fs");
 module.exports.run = async (client, message, args) => {
 	let stats = require(`${client.config.jsonPath}stats.json`);
-	let badgebase = require(`${client.config.jsonPath}/badges.json`);
+	let badgebase = require(`${client.config.jsonPath}badges.json`);
 	try{
 		const argus = message.content.split(' ').slice(1);
 		switch(args[0]){
@@ -12,7 +12,7 @@ module.exports.run = async (client, message, args) => {
 					let noCodename = new MessageEmbed()
 						.setColor(client.config.embedColor)
 						.setTitle(`Значки: синтаксис`)
-						.setDescription(`/lzbdg create <codename> <emoji> <name>|<description>`)
+						.setDescription(`\`/lzbdg create <код> <эмодзи> <название>|<описание>\``)
 						.setTimestamp()
 						.setFooter(message.author.tag, message.author.displayAvatarURL({ dynamic: true }));
 					message.channel.send({embeds: [noCodename]});
@@ -23,16 +23,16 @@ module.exports.run = async (client, message, args) => {
 					codename: args[1],
 					emoji: args[2],
 					name: strings[0],
-					description: strings[1]
+					description: strings[1],
+					manualGiveaway: true
 				});
 				fs.writeFileSync(`${client.config.jsonPath}badges.json`, JSON.stringify(badgebase, null, "\t"));
 				let createdBadge = new MessageEmbed()
 					.setColor(client.config.embedColor)
 					.setTitle(`Значок успешно создан.`)
-					.addField(`Codename`, `${args[1]}`, false)
-					.addField(`Emoji`, `${args[2]}`, false)
-					.addField(`Name`, `${strings[0]}`, false)
-					.addField(`Description`, `${strings[1]}`, false)
+					.addField(`Код`, `\`${args[1]}\``, false)
+					.addField(`Значок`, `${args[2]} ${strings[0]}`, false)
+					.addField(`Описание`, `${strings[1]}`, false)
 					.setTimestamp()
 					.setFooter(message.author.tag, message.author.displayAvatarURL({ dynamic: true }));
 				message.channel.send({embeds: [createdBadge]});
@@ -44,7 +44,7 @@ module.exports.run = async (client, message, args) => {
 					let noCodename = new MessageEmbed()
 						.setColor(client.config.embedColor)
 						.setTitle(`Значки: синтаксис`)
-						.setDescription(`/lzbdg delete <codename>`)
+						.setDescription(`\`/lzbdg delete <код>\``)
 						.setTimestamp()
 						.setFooter(message.author.tag, message.author.displayAvatarURL({ dynamic: true }));
 					message.channel.send({embeds: [noCodename]});
@@ -52,6 +52,16 @@ module.exports.run = async (client, message, args) => {
 				}
 				let badgeCheck = badgebase.findIndex((badge => badge.codename == args[1]));
 				if(badgeCheck != -1){
+					if (badgebase[badgeCheck].manualGiveaway == false) {
+						let manualErr = new MessageEmbed()
+							.setColor(client.config.embedColor)
+							.setTitle(`Ошибка`)
+							.setDescription(`Этот значок не может быть удалён`)
+							.setTimestamp()
+							.setFooter(message.author.tag, message.author.displayAvatarURL({dynamic: true}))
+						message.channel.send({embeds: [manualErr]})
+						break
+					} 
 					badgebase.splice(badgeCheck, 1);
 					fs.writeFileSync(`${client.config.jsonPath}badges.json`, JSON.stringify(badgebase, null, "\t"));
 					let deletedBadge = new MessageEmbed()
@@ -77,23 +87,21 @@ module.exports.run = async (client, message, args) => {
 					let noCodename = new MessageEmbed()
 						.setColor(client.config.embedColor)
 						.setTitle(`Значки: синтаксис`)
-						.setDescription(`/lzbdg info <codename>`)
+						.setDescription(`\`/lzbdg info <код>\``)
 						.setTimestamp()
 						.setFooter(message.author.tag, message.author.displayAvatarURL({ dynamic: true }));
 					message.channel.send({embeds: [noCodename]});
 					break;
 				};
 				try{
-					let bdgEmoji = badgebase.find(badg => badg.codename === args[1]).emoji;
-					let bdgName = badgebase.find(badg => badg.codename === args[1]).name;
-					let bdgDesc = badgebase.find(badg => badg.codename === args[1]).description;
+					let bdg = badgebase.find(badg => badg.codename === args[1]);
 					let fetchedBadge = new MessageEmbed()
 						.setColor(client.config.embedColor)
 						.setTitle(`Информация о значке`)
-						.addField(`Codename`, `${args[1]}`, false)
-						.addField(`Emoji`, `${bdgEmoji}`, false)
-						.addField(`Name`, `${bdgName}`, false)
-						.addField(`Description`, `${bdgDesc}`, false)
+						.addField(`Код`, `\`${args[1]}\``, false)
+						.addField(`Значок`, `${bdg.emoji} ${bdg.name}`, true)
+						.addField(`Доступен?`, `${(bdg.manualGiveaway) ? `да` : `нет`}`, true)
+						.addField(`Описание`, `${bdg.description}`, false)
 						.setTimestamp()
 						.setFooter(message.author.tag, message.author.displayAvatarURL({ dynamic: true }));
 					message.channel.send({embeds: [fetchedBadge]});
@@ -108,11 +116,10 @@ module.exports.run = async (client, message, args) => {
 				let strings = argus.slice(3).join(' ').split(/[|]/g);
 				let testBadge = new MessageEmbed()
 					.setColor(client.config.embedColor)
-					.setTitle(`Arguments`)
-					.addField(`Codename`, `${args[1]}`, false)
-					.addField(`Emoji`, `${args[2]}`, false)
-					.addField(`Name`, `${strings[0]}`, false)
-					.addField(`Description`, `${strings[1]}`, false)
+					.setTitle(`Значок: проверка`)
+					.addField(`Код`, `\`${args[1]}\``, false)
+					.addField(`Значок`, `${args[2]} ${strings[0]}`, false)
+					.addField(`Описание`, `${strings[1]}`, false)
 					.setTimestamp()
 					.setFooter(message.author.tag, message.author.displayAvatarURL({ dynamic: true }));
 				message.channel.send({embeds: [testBadge]});
@@ -122,12 +129,16 @@ module.exports.run = async (client, message, args) => {
 			case 'all': {
 				let list = [];
 				badgebase.map(badge => {
-					list.push(`${badge.codename}`)
+					if (badge.manualGiveaway == false) {
+						list.push(`${badge.emoji} ~~${badge.name} (${badge.codename})~~`)
+					} else {
+						list.push(`${badge.emoji} ${badge.name} (${badge.codename})`)
+					}
 				});
 				let noCodename = new MessageEmbed()
 					.setColor(client.config.embedColor)
 					.setTitle(`Значки: список`)
-					.setDescription(`${list}`)
+					.setDescription(`${list.join(`\n`)}`)
 					.setTimestamp()
 					.setFooter(message.author.tag, message.author.displayAvatarURL({ dynamic: true }));
 				message.channel.send({embeds: [noCodename]});
@@ -161,6 +172,16 @@ module.exports.run = async (client, message, args) => {
 						message.channel.send({embeds: [noUser]});
 						break;
 					}
+					let notAvailable = new MessageEmbed()
+						.setColor(client.config.embedColor)
+						.setTitle(`Ошибка`)
+						.setDescription(`Этот значок не доступен для выдачи.`)
+						.setTimestamp()
+						.setFooter(message.author.tag, message.author.displayAvatarURL({ dynamic: true }));
+					if (found.manualGiveaway == false) {
+						message.channel.send({embeds: [notAvailable]})
+						break;
+					}
 					if(!member.badges.includes(args[2])){
 						let currentBadges = member.badges;
 						if(currentBadges.length == 0) {
@@ -174,7 +195,7 @@ module.exports.run = async (client, message, args) => {
 						let Success = new MessageEmbed()
 								.setColor(client.config.embedColor)
 								.setTitle(`Успешно`)
-								.setDescription(`Значок ${args[2]} добавлен пользователю.`)
+								.setDescription(`Значок \`${args[2]}\` добавлен пользователю.`)
 								.setTimestamp()
 								.setFooter(message.author.tag, message.author.displayAvatarURL({ dynamic: true }));
 						message.channel.send({embeds: [Success]});
@@ -208,6 +229,16 @@ module.exports.run = async (client, message, args) => {
 							message.channel.send(noBadge);
 							break;
 						}
+						let notAvailable = new MessageEmbed()
+							.setColor(client.config.embedColor)
+							.setTitle(`Ошибка`)
+							.setDescription(`Этот значок не доступен для удаления.`)
+							.setTimestamp()
+							.setFooter(message.author.tag, message.author.displayAvatarURL({ dynamic: true }));
+						if (found.manualGiveaway == false) {
+							message.channel.send({embeds: [notAvailable]})
+							break
+						}
 						if(!member.badges.includes(args[2])){
 							let currentBadges = member.badges;
 							if(currentBadges.length == 0) {
@@ -221,7 +252,7 @@ module.exports.run = async (client, message, args) => {
 							let Success = new MessageEmbed()
 								.setColor(client.config.embedColor)
 								.setTitle(`Успешно`)
-								.setDescription(`Значок ${args[2]} добавлен пользователю.`)
+								.setDescription(`Значок \`${args[2]}\` добавлен пользователю.`)
 								.setTimestamp()
 								.setFooter(message.author.tag, message.author.displayAvatarURL({ dynamic: true }));
 							message.channel.send({embeds: [Success]});
@@ -268,6 +299,16 @@ module.exports.run = async (client, message, args) => {
 						message.channel.send(noUser);
 						break;
 					}
+					let notAvailable = new MessageEmbed()
+						.setColor(client.config.embedColor)
+						.setTitle(`Ошибка`)
+						.setDescription(`Этот значок не доступен для удаления.`)
+						.setTimestamp()
+						.setFooter(message.author.tag, message.author.displayAvatarURL({ dynamic: true }));
+					if (found.manualGiveaway == false) {
+						message.channel.send({embeds: [notAvailable]})
+						break
+					}
 					if(member.badges.includes(args[2])){
 						let currentBadges = member.badges;
 						let newBadges = currentBadges.replace(`${args[2]},`, '');
@@ -275,7 +316,7 @@ module.exports.run = async (client, message, args) => {
 						let Success = new MessageEmbed()
 							.setColor(client.config.embedColor)
 							.setTitle(`Успешно`)
-							.setDescription(`Значок ${args[2]} удалён у пользователя.`)
+							.setDescription(`Значок \`${args[2]}\` удалён у пользователя.`)
 							.setTimestamp()
 							.setFooter(message.author.tag, message.author.displayAvatarURL({ dynamic: true }));
 						message.channel.send({embeds: [Success]});
@@ -309,6 +350,16 @@ module.exports.run = async (client, message, args) => {
 							message.channel.send({embeds: [noBadge]});
 							break;
 						}
+						let notAvailable = new MessageEmbed()
+							.setColor(client.config.embedColor)
+							.setTitle(`Ошибка`)
+							.setDescription(`Этот значок не доступен для удаления.`)
+							.setTimestamp()
+							.setFooter(message.author.tag, message.author.displayAvatarURL({ dynamic: true }));
+						if (found.manualGiveaway == false) {
+							message.channel.send({embeds: [notAvailable]})
+							break
+						}
 						if(member.badges.includes(args[2])){
 							let currentBadges = member.badges;
 							let newBadges = currentBadges.replace(`${args[2]},`, '');
@@ -316,7 +367,7 @@ module.exports.run = async (client, message, args) => {
 							let Success = new MessageEmbed()
 								.setColor(client.config.embedColor)
 								.setTitle(`Успешно`)
-								.setDescription(`Значок ${args[2]} удалён у пользователя.`)
+								.setDescription(`Значок \`${args[2]}\` удалён у пользователя.`)
 								.setTimestamp()
 								.setFooter(message.author.tag, message.author.displayAvatarURL({ dynamic: true }));
 							message.channel.send({embeds: [Success]});
@@ -342,7 +393,11 @@ module.exports.run = async (client, message, args) => {
 				let Syntax = new MessageEmbed()
 					.setColor(client.config.embedColor)
 					.setTitle(`Значки: синтаксис`)
-					.addField(`Субкоманда`, `create, info, all, add, remove`, false)
+					.addField(`Субкоманда`, `\`create <код> <эмодзи> <название>|<описание>\` — создать значок
+						\`info <код>\` — информация о значке 
+						\`all\` — список всех значков 
+						\`add <@User> <код>\` — добавить значок пользователю 
+						\`remove <@User> <код>\` — удалить значок у пользователя`, false)
 					.setTimestamp()
 					.setFooter(message.author.tag, message.author.displayAvatarURL({ dynamic: true }));
 				message.channel.send({embeds: [Syntax]});

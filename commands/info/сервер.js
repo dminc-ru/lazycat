11615546cbb969f80bbee0fb3ec0ce14e473256d
Dan <whatdanman@gmail.com>
2022-01-02 21:53:34 +0300

    Обновление до v13 + фикс багов

--- commands/info/сервер.js
+++ commands/info/сервер.js
@@ -30,24 +30,43 @@ module.exports.run = async (client, interaction) => {
 			"VERY_HIGH": "Самый высокий"
 		};
 		let region = {
-			"brazil": ":flag_br: Бразилия",
-			"europe": ":flag_eu: Европа",
-			"singapore": ":flag_sg: Сингапур",
-			"us-central": ":flag_us: Центральная Америка",
-			"sydney": ":flag_au: Сидней",
-			"us-east": ":flag_us: Восточная Америка",
-			"us-south": ":flag_us: Южная Америка",
-			"us-west": ":flag_us: Западная Америка",
-			"eu-west": ":flag_eu: Западная Европа",
-			"vip-us-east": ":flag_us: VIP Восточная Америка",
-			"london": ":flag_gb: Лондон",
-			"amsterdam": ":flag_nl: Амстердам",
-			"hongkong": ":flag_hk: Гонконг",
-			"russia": ":flag_ru: Россия",
-			"southafrica": ":flag_za:  Южная Африка",
-			"india": ":flag_in: Индия",
-			"japan": ":flag_jp: Япония"
+			"en-US": ":flag_us: США",
+			"en-GB": ":flag_gb: Великобритания",
+			"bg": ":flag_bg: Болгария",
+			"zh-CN": ":flag_cn: Китай",
+			"zh-TW": ":flag_cn: Тайвань",
+			"hr": ":flag_hr: Хорватия",
+			"cs": ":flag_cz: Чехословакия",
+			"da": ":flag_dk: Дания",
+			"nl": ":flag_nl: Нидерланды",
+			"fi": ":flag_fi: Финляндия",
+			"fr": ":flag_fr: Франция",
+			"de": ":flag_de: Германия",
+			"el": ":flag_gr: Греция",
+			"hi": ":flag_in: Индия",
+			"hu": ":flag_hu: Венгрия",
+			"it": ":flag_it: Италия",
+			"ja": ":flag_jp: Япония",
+			"ko": ":flag_kr: Корея",
+			"lt": ":flag_lt: Литва",
+			"no": ":flag_no: Норвегия",
+			"pl": ":flag_pl: Польша",
+			"pt-BR": ":flag_br: Бразилия",
+			"ro": ":flag_ro: Румыния",
+			"ru": ":flag_ru: Россия",
+			"es-ES": ":flag_es: Испания",
+			"sv-SE": ":flag_se: Швеция",
+			"th": ":flag_th: Тайланд",
+			"tr": ":flag_tr: Турция",
+			"uk": ":flag_ua: Украина",
+			"vi": ":flag_vn: Вьетнам"
 		};
+		let tier = {
+			"NONE": "-",
+			"TIER_1": "Друзья",
+			"TIER_2": "Группы",
+			"TIER_3": "Сообщества"
+		}
 		moment.locale("ru");
 		const servercreated = moment(guild.createdAt).format('LL');
 		let serverEmbed = new MessageEmbed()
@@ -55,19 +74,19 @@ module.exports.run = async (client, interaction) => {
 			.setTitle(`Информация о сервере ${guild.name}`)
 			.setThumbnail(guild.iconURL())
 			.addField('ID', interaction.guildId, true)
-			.addField('Владелец', `<@${guild.owner.user.id}>`, true)
-			.addField('Регион', region[guild.region], true)
-			.addField('Всего участников', guild.memberCount, true)
-			.addField('Людей', guild.members.cache.filter(member => !member.user.bot).size, true)
-			.addField('Ботов', guild.members.cache.filter(member => member.user.bot).size, true)
+			.addField('Владелец', `<@${guild.ownerId}>`, true)
+			.addField('Регион', region[guild.preferredLocale], true)
+			.addField('Всего участников', `${guild.memberCount}`, true)
+			.addField('Людей онлайн:', `${guild.members.cache.filter(member => !member.user.bot).size}`, true)
+			.addField('Ботов онлайн:', `${guild.members.cache.filter(member => member.user.bot).size}`, true)
 			.addField('Уровень верификации', verifLevels[guild.verificationLevel], true)
-			.addField('Всего каналов', guild.channels.cache.size, true)
-			.addField('Всего ролей', guild.roles.cache.size, true)
+			.addField('Всего каналов', `${guild.channels.cache.size}`, true)
+			.addField('Всего ролей', `${guild.roles.cache.size}`, true)
 			.addField('Дата создания', `${servercreated} (${checkDays(guild.createdAt)})`, true)
-			.addField('Всего бустов', guild.premiumSubscriptionCount, true)
-			.addField('Уровень буста', guild.premiumTier)
+			.addField('Всего бустов', `${guild.premiumSubscriptionCount}`, true)
+			.addField('Уровень буста', `${tier[guild.premiumTier]}`)
 			.setTimestamp()
-			.setFooter(user.tag, user.displayAvatarURL({dynamic: true}))
+			.setFooter({ text: user.tag, iconURL: user.displayAvatarURL({dynamic: true}) })
 		return interaction.reply({embeds: [serverEmbed]})
 	} catch (error) {
 		client.logger.log(error, 'err')

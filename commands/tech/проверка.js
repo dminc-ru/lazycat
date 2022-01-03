8cb9e1a42260760f85d1f865f57e6200390a8516
Dan <whatdanman@gmail.com>
2022-01-03 13:00:23 +0300

    Новое право + .setFooter

--- commands/tech/проверка.js
+++ commands/tech/проверка.js
@@ -17,26 +17,27 @@ module.exports.run = async (client, interaction) => {
 			.setColor(client.config.embedColor)
 			.setTitle('Проверка разрешений для бота')
 			.setDescription(`**Для корректной работы функций необходимы следующие права:**
-				${(guild.me.hasPermission("MANAGE_ROLES")) ? tick : cross} Управлять ролями
-				${(guild.me.hasPermission("MANAGE_CHANNELS")) ? tick : cross} Управлять каналами
-				${(guild.me.hasPermission("KICK_MEMBERS")) ? tick : cross} Выгонять участников
-				${(guild.me.hasPermission("BAN_MEMBERS")) ? tick : cross} Банить участников
-				${(guild.me.hasPermission("MANAGE_NICKNAMES")) ? tick : cross} Управлять никнеймами
-				${(guild.me.hasPermission("CHANGE_NICKNAME")) ? tick : cross} Изменять никнейм
-				${(guild.me.hasPermission("VIEW_AUDIT_LOG")) ? tick : cross} Просматривать журнал аудита
-				${(guild.me.hasPermission("MANAGE_MESSAGES")) ? tick : cross} Управлять сообщениями
-				${(guild.me.hasPermission("ATTACH_FILES")) ? tick : cross} Прикреплять файлы
-				${(guild.me.hasPermission("READ_MESSAGE_HISTORY")) ? tick : cross} Просматривать историю сообщений
-				${(guild.me.hasPermission("ADD_REACTIONS")) ? tick : cross} Добавлять реакции
-				${(guild.me.hasPermission("USE_EXTERNAL_EMOJIS")) ? tick : cross} Использовать внешние эмодзи
-				${(guild.me.hasPermission("CONNECT")) ? tick : cross} Подключаться
-				${(guild.me.hasPermission("SPEAK")) ? tick : cross} Говорить
-				${(guild.me.hasPermission("MUTE_MEMBERS")) ? tick : cross} Отключать участникам микрофон
-				${(guild.me.hasPermission("DEAFEN_MEMBERS")) ? tick : cross} Отключать участникам звук
-				${(guild.me.hasPermission("USE_VAD")) ? tick : cross} Просматривать активность голосового чата`
+				${(guild.me.permissions.has("MANAGE_ROLES")) ? tick : cross} Управлять ролями
+				${(guild.me.permissions.has("MANAGE_CHANNELS")) ? tick : cross} Управлять каналами
+				${(guild.me.permissions.has("KICK_MEMBERS")) ? tick : cross} Выгонять участников
+				${(guild.me.permissions.has("BAN_MEMBERS")) ? tick : cross} Банить 
+				${(guild.me.permissions.has("MODERATE_MEMBERS")) ? tick: cross} Отправлять участников подумать о своём поведении
+				${(guild.me.permissions.has("MANAGE_NICKNAMES")) ? tick : cross} Управлять никнеймами
+				${(guild.me.permissions.has("CHANGE_NICKNAME")) ? tick : cross} Изменять никнейм
+				${(guild.me.permissions.has("VIEW_AUDIT_LOG")) ? tick : cross} Просматривать журнал аудита
+				${(guild.me.permissions.has("MANAGE_MESSAGES")) ? tick : cross} Управлять сообщениями
+				${(guild.me.permissions.has("ATTACH_FILES")) ? tick : cross} Прикреплять файлы
+				${(guild.me.permissions.has("READ_MESSAGE_HISTORY")) ? tick : cross} Просматривать историю сообщений
+				${(guild.me.permissions.has("ADD_REACTIONS")) ? tick : cross} Добавлять реакции
+				${(guild.me.permissions.has("USE_EXTERNAL_EMOJIS")) ? tick : cross} Использовать внешние эмодзи
+				${(guild.me.permissions.has("CONNECT")) ? tick : cross} Подключаться
+				${(guild.me.permissions.has("SPEAK")) ? tick : cross} Говорить
+				${(guild.me.permissions.has("MUTE_MEMBERS")) ? tick : cross} Отключать участникам микрофон
+				${(guild.me.permissions.has("DEAFEN_MEMBERS")) ? tick : cross} Отключать участникам звук
+				${(guild.me.permissions.has("USE_VAD")) ? tick : cross} Просматривать активность голосового чата`
 			)
 			.setTimestamp()
-			.setFooter(user.tag, user.displayAvatarURL({dynamic: true}))
+			.setFooter({ text: user.tag, iconURL: user.displayAvatarURL({dynamic: true}) })
 		return interaction.reply({embeds: [checkEmbed]})
 	} catch(error) {
 		client.logger.log(error, 'err')

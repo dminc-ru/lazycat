57d0c6581d2cfe347d64bc9a601dc76698a49f92
Dan <whatdanman@gmail.com>
2022-01-04 22:19:06 +0300

    Фикс багов

--- commands/music/плей.js
+++ commands/music/плей.js
@@ -1,8 +1,11 @@
 const { QueryType } = require('discord-player');
+const { MessageEmbed } = require('discord.js');
 module.exports.run = async (client, interaction) => {
     await interaction.deferReply();
+    var user = client.users.cache.get(interaction.member.user.id)
     const guild = client.guilds.cache.get(interaction.guildId);
     const channel = guild.channels.cache.get(interaction.channelId);
+    const playNext = interaction.options.getString('следующий')
     const query = interaction.options.getString('запрос');
     const searchResult = await client.player
         .search(query, {
@@ -10,29 +13,50 @@ module.exports.run = async (client, interaction) => {
             searchEngine: QueryType.AUTO
         })
         .catch(() => {
-            console.log('he');
+            console.log('wtf man');
         });
-    if (!searchResult || !searchResult.tracks.length) return void interaction.followUp({ content: 'Ничего не найдено.' });
-
-    const queue = await client.player.createQueue(guild, {
-        ytdlOptions: {
-            filter: 'audioonly',
-            highWaterMark: 1 << 30,
-            dlChunkSize: 0,
-        },
-        metadata: channel
-    });
+    let notFound = new MessageEmbed()
+        .setColor(client.config.embedColor)
+        .setTitle('Ничего не найдено.')
+        .setTimestamp()
+        .setFooter({ text: user.tag, iconURL: user.displayAvatarURL({dynamic: true}) })
+    if (!searchResult || !searchResult.tracks.length) return void interaction.followUp({ embeds: [notFound] });
+    const queue = client.player.getQueue(interaction.guildId);
+    if (!queue || !queue.playing) {
+        var queue = await client.player.createQueue(guild, {
+            ytdlOptions: {
+                filter: 'audioonly',
+                highWaterMark: 1 << 30,
+                dlChunkSize: 0,
+            },
+            metadata: channel
+        });
+    }
 
     const member = guild.members.cache.get(interaction.member.user.id) ?? await guild.members.fetch(interaction.member.user.id);
     try {
         if (!queue.connection) await queue.connect(member.voice.channel);
     } catch {
-        void client.player.deleteQueue(ctx.guildID);
-        return void interaction.followUp({ content: 'Не могу присоединиться к голосовому каналу.' });
+        void client.player.deleteQueue(interaction.guildId);
+        let cantJoin = new MessageEmbed()
+            .setColor(client.config.embedColor)
+            .setTitle('Ошибка')
+            .setDescription('Не могу присоединиться к голосовому каналу.')
+            .setTimestamp()
+            .setFooter({ text: user.tag, iconURL: user.displayAvatarURL({dynamic: true}) })
+        return void interaction.followUp({ embeds: [cantJoin] });
     }
-
-        await interaction.followUp({ content: `Загружается ${searchResult.playlist ? 'плейлист' : 'трек'}...` });
-        searchResult.playlist ? queue.addTracks(searchResult.tracks) : queue.addTrack(searchResult.tracks[0]);
+        let loadingEmbed = new MessageEmbed()
+            .setColor(client.config.embedColor)
+            .setTitle(`Загрузка ${searchResult.playlist ? 'плейлиста' : 'трека'}...`)
+            .setTimestamp()
+            .setFooter({ text: user.tag, iconURL: user.displayAvatarURL({dynamic: true}) })
+        await interaction.followUp({ embeds: [loadingEmbed] });
+        if (!playNext || !queue.playing) {
+            searchResult.playlist ? queue.addTracks(searchResult.tracks) : queue.addTrack(searchResult.tracks[0]);
+        } else {
+            queue.insert(searchResult.tracks[0]);
+        }
         if (!queue.playing) await queue.play();
 }
 

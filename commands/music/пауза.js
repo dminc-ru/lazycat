7ae9a9a8029ba8d1b6693a6bbfa628e937ecd475
Dan <whatdanman@gmail.com>
2022-01-04 22:58:42 +0300

    Добавлена команда «пауза»

---
+++ commands/music/пауза.js
@@ -0,0 +1,24 @@
+module.exports.run = async (client, interaction) => {
+    await interaction.deferReply();
+
+    const queue = client.player.getQueue(interaction.guildId);
+    let noMusic = new MessageEmbed()
+        .setColor(client.config.embedColor)
+        .setTitle('Очередь пуста.')
+        .setTimestamp()
+        .setFooter({ text: user.tag, iconURL: user.displayAvatarURL({dynamic: true}) })
+    if (!queue || !queue.playing) return void interaction.followUp({ embeds: [noMusic] });
+    const paused = queue.setPaused(true);
+    let paused = new MessageEmbed()
+        .setColor(client.config.embedColor)
+        .setTitle(`${paused ? `Приостановлено.` : `Произошла ошибка.`}`)
+        .setTimestamp()
+        .setFooter({ text: user.tag, iconURL: user.displayAvatarURL({dynamic: true}) })
+    return void interaction.followUp({ embeds: [paused] });
+}
+
+module.exports.data = {
+    name: "пауза",
+    permissions: ["tester"],
+    type: "interaction"
+}

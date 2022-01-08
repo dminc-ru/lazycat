38e7c5a6024ba59d59a4b4b6f197bc5d3e154c67
Dan <whatdanman@gmail.com>
2022-01-08 22:15:58 +0300

    Смена библиотеки sqlite3

--- utils/db.js
+++ utils/db.js
@@ -1,78 +1,61 @@
-const sqlite3 = require('sqlite3').verbose();
 const config = require('../config.js')
 module.exports = class db {
     static addUser (id) {
         return new Promise((resolve, reject) => {
-            var db = new sqlite3.Database(config.databasePath);
-            db.serialize(() => {
-                db.run(`INSERT INTO users (discord_id) VALUES (?)`, [id])
-				        db.run(`UPDATE users SET (join_date) = ? WHERE discord_id = ${id}`, [Date.now()]);
-            })
-            db.close()
+            const database = require('better-sqlite3')(config.databasePath)
+            database.prepare('INSERT INTO users (discord_id) VALUES (?)').run(id)
+            database.prepare(`UPDATE users SET (join_date) = ? WHERE discord_id = ${id}`).run(Date.now())
+            database.close()
             resolve()
         });
     }
     static addGuild (id) {
         return new Promise((resolve, reject) => {
-            var db = new sqlite3.Database(config.databasePath);
-            db.serialize(() => {
-                db.run(`INSERT INTO guilds (discord_id) VALUES (?)`, [id])
-                db.run (`UPDATE guilds SET (join_date) = ? WHERE discord_id = ${id}`, [Date.now()]);
-            })
+            const database = require('better-sqlite3')(config.databasePath)
+            database.prepare('INSERT INTO guilds (discord_id) VALUES (?)').run(id)
+            database.prepare(`UPDATE guilds SET (join_date) = ? WHERE discord_id = ${id}`).run(Date.now())
+            database.close()
+            resolve()
         })
     }
     static getUser (id) {
         return new Promise((resolve, reject) => {
-            var db = new sqlite3.Database(config.databasePath);
-            db.serialize(() => {
-                db.all(`SELECT * FROM users WHERE discord_id = ${id}`, (err, row) => {
-                    if (err) console.log(err)
-                    else resolve(row[0]);
-                })
-            })
+            const database = require('better-sqlite3')(config.databasePath)
+            const row = database.prepare(`SELECT * FROM users WHERE discord_id = ?`).get(id)
+            database.close()
+            resolve(row)
         })
     }
     static getGuild (id) {
         return new Promise((resolve, reject) => {
-            var db = new sqlite3.Database(config.databasePath);
-            db.serialize(() => {
-                db.all(`SELECT * FROM guilds WHERE discord_id = ${id}`, (err, row) => {
-                    if (err) console.log(err)
-                    else resolve(row[0]);
-                })
-            })
+            const database = require('better-sqlite3')(config.databasePath)
+            const row = database.prepare(`SELECT * FROM guilds WHERE discord_id = ?`).get(id)
+            database.close()
+            resolve(row)
         })
     }
     static changeUser (id, column, new_value) {
         return new Promise((resolve, reject) => {
-            var db = new sqlite3.Database(config.databasePath);
-            db.serialize(() => {
-                db.run(`UPDATE users SET ${column} = ? WHERE discord_id = ${id}`, [new_value]);
-            });
-            db.close()
+            const database = require('better-sqlite3')(config.databasePath)
+            database.prepare(`UPDATE users SET ${column} = ? WHERE discord_id = ${id}`).run(new_value)
+            database.close()
             resolve()
         })
     }
     static changeGuild (id, column, new_value) {
         return new Promise((resolve, reject) => {
-            var db = new sqlite3.Database(config.databasePath);
-            db.serialize(() => {
-                db.run(`UPDATE guilds SET ${column} = ? WHERE discord_id = ${id}`, [new_value]);
-            });
-            db.close()
+            const database = require('better-sqlite3')(config.databasePath)
+            database.prepare(`UPDATE guilds SET ${column} = ? WHERE discord_id = ${id}`).run(new_value)
+            database.close()
             resolve()
         })
     }
     static getTable (table) {
         return new Promise((resolve, reject) => {
-            var db = new sqlite3.Database(config.databasePath);
-            db.serialize(() => {
-                db.all(`SELECT * FROM ${table}`, (err, row) => {
-                    if(err) console.log(err)
-                    else resolve(row)
-                })
-            })
-            db.close()
+            const database = require('better-sqlite3')(config.databasePath)
+            const row = database.prepare(`SELECT * FROM ${table}`).all()
+            database.close()
+            resolve(row)
         })
     }
 }

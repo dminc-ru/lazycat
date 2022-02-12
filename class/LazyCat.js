d6be847cf1fc8220515ed0b91c8b0f421801a2da
Dan <whatdanman@gmail.com>
2022-02-12 21:16:16 +0300

    Init commit

---
+++ class/LazyCat.js
@@ -0,0 +1,33 @@
+const { Client, Collection, Intents } = require('discord.js');
+const { Player } = require('discord-player');
+class LazyCat extends Client {
+    constructor() {
+        super({
+            intents: [
+                Intents.FLAGS.GUILDS,
+                Intents.FLAGS.GUILD_MESSAGES,
+                Intents.FLAGS.GUILD_MEMBERS,
+                Intents.FLAGS.DIRECT_MESSAGES,
+                Intents.FLAGS.GUILD_VOICE_STATES
+            ]
+        });
+        this.player = new Player(this, {
+			leaveOnEmpty: false,
+			enableLive: true
+		});
+        this.config = require('./config');
+        this.commands = new Collection();
+        this.logger = require('./utils/logger');
+        this.db = require('./utils/db');
+        this.emoji = require('./emojis');
+        this.messages = require('./messages');
+        this.queue = new Map();
+        this.stats = require(`${client.config.jsonPath}stats.json`);
+        this.exchange = require(`${client.config.jsonPath}exchange.json`);
+        this.shop = require(`${client.config.jsonPath}shop.json`);
+    };
+    saveJSON (name) {
+        nameFile = () => Object.keys({ name })[0];
+        fs.writeFileSync(`${this.config.jsonPath}${name}.json`, JSON.stringify(name, null, "\t"));
+    }
+}

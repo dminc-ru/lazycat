729cb1478c8ca8a6636fb19faef4fb83060a865c
Dan <dan@dminc.ru>
2022-02-19 21:39:45 +0300

    Embed Creation

--- commands/economy/банк.js
+++ commands/economy/банк.js
@@ -2,10 +2,7 @@ const { MessageEmbed } = require('discord.js')
 module.exports.run = async (client, interaction) => {
 	try {
 		let userdb = await client.db.getUser(interaction.member.user.id)
-		let noUser = new MessageEmbed()
-			.setColor(client.config.embedColor)
-			.setTitle('Ошибка')
-			.setDescription('Пользователь не найден в базе данных.')
+		let noUser = client.utils.error('Пользователь не найден в базе данных.')
 		try {
 			var user = await client.users.fetch(interaction.member.user.id);
 		} catch (error) {
@@ -30,12 +27,7 @@ module.exports.run = async (client, interaction) => {
 				}
 				client.db.changeUser(interaction.member.user.id, 'balance_fish', (userdb.balance_fish - money))
 				client.db.changeUser(interaction.member.user.id, 'balance_bank', (userdb.balance_bank + money))
-				let successEmbed = new MessageEmbed()
-					.setColor(client.config.embedColor)
-					.setTitle('Транзакция')
-					.setDescription(`Успешно! На ваш счёт зачислено ${money} ${client.emoji.fish}`)
-					.setTimestamp()
-					.setFooter({ text: user.tag, iconURL: user.displayAvatarURL({dynamic: true}) })
+				let successEmbed = client.utils.success(`На ваш счёт зачислено ${money} ${client.emoji.fish}`, user)
 				interaction.reply({embeds: [successEmbed]})
 				break
 			}
@@ -51,65 +43,39 @@ module.exports.run = async (client, interaction) => {
 				}
 				client.db.changeUser(interaction.member.user.id, 'balance_fish', (userdb.balance_fish + money))
 				client.db.changeUser(interaction.member.user.id, 'balance_bank', (userdb.balance_bank - money))
-				let successEmbed = new MessageEmbed()
-					.setColor(client.config.embedColor)
-					.setTitle('Транзакция')
-					.setDescription(`Успешно! Вы сняли ${money} ${client.emoji.fish} с вашего счёта.`)
-					.setTimestamp()
-					.setFooter({ text: user.tag, iconURL: user.displayAvatarURL({dynamic: true}) })
+				let successEmbed = client.utils.success(`Успешно! Вы сняли ${money} ${client.emoji.fish} с вашего счёта.`, user)
 				interaction.reply({embeds: [successEmbed]})
 				break
 			}
 			case 'ограбить': {
-				function randomBank(min, max) {
-					let rand = min - 0.5 + Math.random() * (max - min + 1);
-					return Math.round(rand);
-				}
 				var generatedCode = "";
-				let firstNumber = randomBank(1, 9);
+				let firstNumber = client.utils.randInt(1, 9);
 				generatedCode += firstNumber;
-				let secondNumber = randomBank(1, 9);
+				let secondNumber = client.utils.randInt(1, 9);
 				generatedCode += secondNumber;
-				let thirdNumber = randomBank(1, 9);
+				let thirdNumber = client.utils.randInt(1, 9);
 				generatedCode += thirdNumber;
-				let fourthNumber = randomBank(1, 9);
+				let fourthNumber = client.utils.randInt(1, 9);
 				var guessNumber = String(fourthNumber)
 				generatedCode += "[]"
-				let fiveNumber = randomBank(1, 9);
+				let fiveNumber = client.utils.randInt(1, 9);
 				generatedCode += fiveNumber;
-				let robberyEmbed = new MessageEmbed()
-					.setColor(client.config.embedColor)
-					.setTitle('Банк: ограбление')
-					.setDescription(`Вы подошли к хранилищу банка и достали кусок бумаги, купленный в DarkCat.
-					\`${generatedCode}\`
-					Угадайте цифру кода.`)
-					.setTimestamp()
-					.setFooter({ text: user.tag, iconURL: user.displayAvatarURL({dynamic: true}) })
+				let robberyEmbed = client.utils.embed('Банк: ограбление', `Вы подошли к хранилищу банка и достали кусок бумаги, купленный в DarkCat. \`${generatedCode}\`\nУгадайте цифру кода.`, user)
 				interaction.reply({embeds: [robberyEmbed]})
 				const filter = message => message.author.id === interaction.member.user.id;
 				const collector = interaction.channel.createMessageCollector({ filter, max: 1, time: 30000 });
 				var answered = false;
 				collector.on('collect', async m => {
 					if (m.content == guessNumber) {
-						let reward = randomBank(150, 200);
+						let reward = client.utils.randInt(150, 200);
 						userdb = await client.db.getUser(interaction.member.user.id)
 						client.db.changeUser(interaction.member.user.id, 'balance_fish', (userdb.balance_fish + reward))
-						let successEmbed = new MessageEmbed()
-							.setColor(client.config.embedColor)
-							.setTitle(`Банк: ограбление`)
-							.setDescription(`Код введён. Хранилище открылось! Вы забрали ${reward} ${client.emoji.fish}`)
-							.setTimestamp()
-							.setFooter({ text: user.tag, iconURL: user.displayAvatarURL({dynamic: true}) })
+						let successEmbed = client.utils.embed('Банк: ограбление', `Код введён. Хранилище открылось! Вы забрали ${reward} ${client.emoji.fish}`, user)
 						interaction.editReply({embeds: [successEmbed]})
 						answered = true
 						return collector.stop()
 					} else {
-						let failEmbed = new MessageEmbed()
-							.setColor(client.config.embedColor)
-							.setTitle('Банк: ограбление')
-							.setDescription(`Код введён. Сработала сигнализация! Вы смогли убежать от охраны этого банка.`)
-							.setTimestamp()
-							.setFooter({ text: user.tag, iconURL: user.displayAvatarURL({dynamic: true}) })
+						let failEmbed = client.utils.embed('Банк: ограбление', `Код введён. Сработала сигнализация! Вы смогли убежать от охраны этого банка.`, user)
 						interaction.editReply({embeds: [failEmbed]})
 						answered = true
 						return collector.stop()
@@ -118,12 +84,7 @@ module.exports.run = async (client, interaction) => {
 	
 				collector.on('end', collected => {
 					if (!answered) {
-						let failEmbed = new MessageEmbed()
-							.setColor(client.config.embedColor)
-							.setTitle('Банк: ограбление')
-							.setDescription(`Пока вы думали, вас заметила охрана этого банка. Вы смогли убежать.`)
-							.setTimestamp()
-							.setFooter({ text: user.tag, iconURL: user.displayAvatarURL({dynamic: true}) })
+						let failEmbed = client.utils.embed('Банк: ограбление', `Пока вы думали, вас заметила охрана этого банка. Вы смогли убежать.`, user)
 						return interaction.editReply({embeds: [failEmbed]})
 					} else { 
 						return

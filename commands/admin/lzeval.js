4e338f1b65012ce960f202cfb4cce61f4ae17154
Dan <dan@dminc.ru>
2022-02-19 11:08:43 +0300

    Fix string replacements, added Embed Creation

--- commands/admin/lzeval.js
+++ commands/admin/lzeval.js
@@ -9,41 +9,37 @@ module.exports.run = async (client, message, args) => {
 		if (typeof output !== "string") {
 			output = require('util').inspect(output, { depth: 0 });
 		}
-		output.replace(client.config.token, "[REDACTED]")
-		output.replace(client.config.webhookBan.id, "[REDACTED]")
-		output.replace(client.config.webhookBan.token, "[REDACTED]")
-		output.replace(client.config.secretKey, "[REDACTED]")
+		output.replaceAll(client.config.token, "[REDACTED]")
+		output.replaceAll(client.config.webhookBan.id, "[REDACTED]")
+		output.replaceAll(client.config.webhookBan.token, "[REDACTED]")
+		output.replaceAll(client.config.secretKey, "[REDACTED]")
 		if (output.length > (1024 - (message.content.length))) {
 			client.logger.log(`Evaluated code via /lzeval`, 'debug')
 			return console.log(output)
 		}
-		let embedEval = new MessageEmbed()
-			.setColor(client.config.embedColor)
+		let embedEval = client.utils.embed(undefined, undefined, message.author)
 			.addField(':inbox_tray: Input:', `\`\`\`js\n${beautify(code)}\n\`\`\``, false)
 			.addField(':outbox_tray: Output:', `\`\`\`js\n${output}\n\`\`\``, false)
-			.setFooter({ text: message.author.tag, iconURL: message.author.displayAvatarURL({ dynamic: true }) });
       	message.channel.send({embeds: [embedEval]});
 	}).catch((err) => {
 		err = err.toString()
 		if (err.includes(client.config.token)) {
-			err.replace(client.config.token, "[REDACTED]")
+			err.replaceAll(client.config.token, "[REDACTED]")
 		}
 		if (err.includes(client.config.webhookBanToken)) {
-			err.replace(client.config.webhookBanToken, "[REDACTED]")
+			err.replaceAll(client.config.webhookBanToken, "[REDACTED]")
 		}
 		if (err.includes(client.config.secretKey)) {
-			err.replace(client.config.secretKey, "[REDACTED]")
+			err.replaceAll(client.config.secretKey, "[REDACTED]")
 		}
 		if (err.length > (1024 - (message.content.length))) {
 			client.logger.log(`Evaluated code via /lzeval`, 'debug')
 			client.logger.log(`Error!`, 'err')
 			return console.error(output)
 		}
-		let embedEvalErr = new MessageEmbed()
-			.setColor(client.config.embedColor)
+		let embedEvalErr = client.utils.embed(undefined, undefined, message.author)
 			.addField(':inbox_tray: Input:', `\`\`\`js\n${beautify(code)}\n\`\`\``, false)
 			.addField(':outbox_tray: Output:', `\`\`\`js\n${err}\n\`\`\``, false)
-			.setFooter({ text: message.author.tag, iconURL: message.author.displayAvatarURL({ dynamic: true }) });
       	message.channel.send({embeds: [embedEvalErr]});
 	})
 }

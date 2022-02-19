21183a8d006e864f03ef1a57baf09ecdb59d19c4
Dan <dan@dminc.ru>
2022-02-19 11:12:34 +0300

    Add Embed Creation

--- commands/admin/lzping.js
+++ commands/admin/lzping.js
@@ -3,11 +3,9 @@ const ms = require("ms");
 const os = require('os');
 module.exports.run = async (client, message, args) => {
 	try {
-		let stats = require(`${client.config.jsonPath}stats.json`);
+		let stats = client.json.stats
 		var busymem = (((os.totalmem()/(1024*1024*1024)).toFixed(2)) - ((os.freemem()/(1024*1024*1024).toFixed(2))))
-		let pongEmbed = new MessageEmbed()
-			.setColor(client.config.embedColor)
-			.setTitle(`Lazy Cat: server information`)
+		let pongEmbed = client.utils.embed('Lazy Cat: server information')
 			.addField(`Пинг:`, `\`\`\`${Math.round(client.ws.ping)}мс\`\`\``, true)
 			.addField(`Серверов в кэше:`, `\`\`\`${client.guilds.cache.size}\`\`\``, true)
 			.addField(`Пользователей в кэше:`, `\`\`\`${client.users.cache.size}\`\`\``, true)
@@ -15,8 +13,6 @@ module.exports.run = async (client, message, args) => {
 			.addField(`RAM:`, `\`\`\`${busymem.toFixed(2)}GB/${(os.totalmem()/(1024*1024*1024)).toFixed(2)}GB\`\`\``, true)
 			.addField(`Скорость CPU`, `\`\`\`${(os.cpus()[0].speed / 1000).toFixed(2)} GHz\`\`\``, true)
 			.addField(`Модель CPU:`, `\`\`\`${os.cpus()[0].model}\`\`\``, false)
-			.setTimestamp()
-			.setFooter({ text: `${stats.version}`, iconURL: client.user.avatarURL() });
 		message.channel.send({embeds: [pongEmbed]});
 	} catch (error) {
 		client.logger.log(error, 'err')

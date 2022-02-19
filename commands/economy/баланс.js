f27e9cd778c17566a2d6729722bb252aabd9bc9a
Dan <dan@dminc.ru>
2022-02-19 21:39:39 +0300

    Embed Creation

--- commands/economy/баланс.js
+++ commands/economy/баланс.js
@@ -1,25 +1,17 @@
-const { MessageEmbed } = require('discord.js')
 module.exports.run = async (client, interaction) => {
 	try {
-		let noUser = new MessageEmbed()
-			.setColor(client.config.embedColor)
-			.setTitle('Ошибка')
-			.setDescription('Пользователь не найден в базе данных.')
+		let noUser = client.utils.error('Пользователь не найден в базе данных.')
 		try {
 			var user = await client.users.fetch(interaction.member.user.id);
 		} catch (error) {
 			return interaction.reply({embeds: [noUser], ephemeral: true})
 		}
 		let userdb = await client.db.getUser(interaction.member.user.id)
-		let userProfile = new MessageEmbed()
-			.setColor(client.config.embedColor)
-			.setTitle('Баланс')
+		let userProfile = client.utils.embed('Баланс', undefined, user)
 			.setAuthor({name: user.tag, iconURL: user.displayAvatarURL({dynamic: true}) })
 			.addField(`Рыбки:`, `${userdb.balance_fish} ${client.emoji.fish}`, true)
 			.addField(`Жучки:`, `${userdb.balance_bugs} ${client.emoji.bug}`, true)
 			.addField(`В банке:`, `${userdb.balance_bank} ${client.emoji.fish}`, false)
-			.setTimestamp()
-			.setFooter({ text: user.tag, iconURL: user.displayAvatarURL({dynamic: true}) })
 		interaction.reply({embeds: [userProfile]})
 	} catch(error) {
 		client.logger.log(error, 'err')

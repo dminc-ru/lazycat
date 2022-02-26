5ca92ed7244322049d8c9a932b2156d66b9745bd
Dan <dan@dminc.ru>
2022-02-26 20:13:38 +0300

    Embed Creation + little code refactoring

--- commands/economy/жучки.js
+++ commands/economy/жучки.js
@@ -1,12 +1,7 @@
-const fs = require("fs");
-const { MessageEmbed } = require('discord.js')
 module.exports.run = async (client, interaction) => {
 	try {
-		let exchange = require(`${client.config.jsonPath}exchange.json`);
-		let noUser = new MessageEmbed()
-			.setColor(client.config.embedColor)
-			.setTitle('Ошибка')
-			.setDescription('Пользователь не найден в базе данных.')
+		let exchange = client.json.exchange
+		let noUser = client.utils.error('Пользователь не найден в базе данных.')
 		try {
 			var user = await client.users.fetch(interaction.member.user.id);
 		} catch (error) {
@@ -18,58 +13,51 @@ module.exports.run = async (client, interaction) => {
 			return interaction.reply({content: `Укажите корректное количество ${client.emoji.bug}`, ephemeral: true})
 		if(userdb.balance_fish < count)
 			return interaction.reply({content: `У вас недостаточно средств.`, ephemeral: true})
-		if(whattoDo == "купить"){
-			userdb = await client.db.getUser(interaction.member.user.id)	
-			let clientdb = await client.db.getUser(client.user.id)
+		switch (whattoDo) {
+			case 'купить': {
+				userdb = await client.db.getUser(interaction.member.user.id)	
+				let clientdb = await client.db.getUser(client.user.id)
 
-			var bugs = interaction.options.getInteger('количество');
-			var count = bugs * exchange.currentBugPrice; 
+				var bugs = interaction.options.getInteger('количество');
+				var count = bugs * exchange.currentBugPrice; 
 
-			client.db.changeUser(interaction.member.user.id, 'balance_fish', (userdb.balance_fish - count))
-			client.db.changeUser(client.user.id, 'balance_fish', (clientdb.balance_fish + count))
-			client.db.changeUser(interaction.member.user.id, 'balance_bugs', (userdb.balance_bugs + bugs))
-			exchange.boughtBugs += bugs;
-			fs.writeFileSync(`${client.config.jsonPath}exchange.json`, JSON.stringify(exchange, null, "\t"));
-			let successEmbed = new MessageEmbed()
-				.setColor(client.config.embedColor)
-				.setTitle(`Транзакция`)
-				.setDescription(`Успешно! Куплено ${bugs} ${client.emoji.bug} за ${count} ${client.emoji.fish}`)
-				.setTimestamp()
-				.setFooter({ text: user.tag, iconURL: user.displayAvatarURL({dynamic: true}) })
-			return interaction.reply({embeds: [successEmbed]})
-		}
-		if(whattoDo == "продать"){
-			userdb = await client.db.getUser(interaction.member.user.id)
-			let clientdb = await client.db.getUser(client.user.id)
+				client.db.changeUser(interaction.member.user.id, 'balance_fish', (userdb.balance_fish - count))
+				client.db.changeUser(client.user.id, 'balance_fish', (clientdb.balance_fish + count))
+				client.db.changeUser(interaction.member.user.id, 'balance_bugs', (userdb.balance_bugs + bugs))
+				exchange.boughtBugs += bugs;
+				await client.saveJSON('exchange', exchange)
+				let successEmbed = client.utils.success(`Успешно! Куплено ${bugs} ${client.emoji.bug} за ${count} ${client.emoji.fish}`, user)
+				interaction.reply({embeds: [successEmbed]});
+				break;
+			}
+			case 'продать': {
+				userdb = await client.db.getUser(interaction.member.user.id)
+				let clientdb = await client.db.getUser(client.user.id)
 
-			var bugs = interaction.options.getInteger('количество');
-			var count = bugs * exchange.currentBugPrice; 
+				var bugs = interaction.options.getInteger('количество');
+				var count = bugs * exchange.currentBugPrice; 
 
-			client.db.changeUser(interaction.member.user.id, 'balance_bugs', (userdb.balance_bugs - bugs))
-			client.db.changeUser(client.user.id, 'balance_bugs', (clientdb.balance_bugs + bugs))
-			client.db.changeUser(interaction.member.user.id, 'balance_fish', (userdb.balance_fish + count))
-			exchange.sellBugs += bugs;
-			fs.writeFileSync(`${client.config.jsonPath}exchange.json`, JSON.stringify(exchange, null, "\t"));
-			let successEmbed = new MessageEmbed()
-				.setColor(client.config.embedColor)
-				.setTitle(`Транзакция`)
-				.setDescription(`Успешно! Продано ${bugs} ${client.emoji.bug} за ${count} ${client.emoji.fish}`)
-				.setTimestamp()
-				.setFooter({ text: user.tag, iconURL: user.displayAvatarURL({dynamic: true}) })
-			return interaction.reply({embeds: [successEmbed]})
-		}
-		if(whattoDo == "курс"){
-			let exchangeEmbed = new MessageEmbed()
-				.setColor(client.config.embedColor)
-				.setTitle(`Жучки`)
-				.setDescription(`Текущий курс обмена жучков:
+				client.db.changeUser(interaction.member.user.id, 'balance_bugs', (userdb.balance_bugs - bugs))
+				client.db.changeUser(client.user.id, 'balance_bugs', (clientdb.balance_bugs + bugs))
+				client.db.changeUser(interaction.member.user.id, 'balance_fish', (userdb.balance_fish + count))
+				exchange.sellBugs += bugs;
+				await client.saveJSON('exchange', exchange)
+				let successEmbed = client.utils.success(`Успешно! Продано ${bugs} ${client.emoji.bug} за ${count} ${client.emoji.fish}`, user)
+				interaction.reply({embeds: [successEmbed]})
+				break;
+			}
+			case 'курс': {
+				let exchangeEmbed = client.utils.embed(
+					'Жучки', 
+					`Текущий курс обмена жучков:
 					1 ${client.emoji.bug} = ${exchange.currentBugPrice} ${client.emoji.fish}
-
+	
 					/жучки купить <кол-во>
-					/жучки продать <кол-во>`)
-				.setTimestamp()
-				.setFooter({ text: user.tag, iconURL: user.displayAvatarURL({dynamic: true}) })
-			return interaction.reply({embeds: [exchangeEmbed]})
+					/жучки продать <кол-во>`, user)
+				interaction.reply({embeds: [exchangeEmbed]})
+				break
+			}
+			default: break;
 		}
 	} catch(error) {
 		client.logger.log(error, 'err')

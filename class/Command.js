8db60fefa9391985e3b36292179625c7dd479b6d
Dan <dan@dminc.ru>
2022-03-08 15:43:37 +0300

    Modify Command

--- class/Command.js
+++ class/Command.js
@@ -2,7 +2,9 @@ module.exports = class Command {
 	constructor(client, {
 		name = null,
 		permissions = new Array(),
-		type = null
+		type = null,
+        enabled = true,
+        guildOnly = false
 	})
 	{
 		this.data = { name, permissions, type };

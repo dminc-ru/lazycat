06d68fed0e81d47574f20a65dd37252c1c96e438
Dan <dan@dminc.ru>
2022-03-08 15:20:11 +0300

    Embed Creation + little code refactoring

--- commands/info/инфо.js
+++ commands/info/инфо.js
@@ -1,21 +1,15 @@
-const { MessageEmbed } = require('discord.js')
 module.exports.run = async (client, interaction) => {
 	try {
-		let stats = require(`${client.config.jsonPath}stats.json`);
-		let noUser = new MessageEmbed()
-			.setColor(client.config.embedColor)
-			.setTitle('Ошибка')
-			.setDescription('Пользователь не найден в базе данных.')
+		let stats = client.json.stats
+		let noUser = client.utils.error('Пользователь не найден в базе данных.')
 		try {
 			var user = await client.users.fetch(interaction.member.user.id);
 		} catch (error) {
 			return message.channel.send({embeds: [noUser]})
 		}
 		var members = client.guilds.cache.reduce((a, g) => a + g.memberCount, 0);
-		let infoEmbed = new MessageEmbed()
-			.setColor(client.config.embedColor)
-			.setTitle('Информация о боте')
-			.setDescription(`${client.emoji.b} Lazy Cat — универсальный бот для вашего сервера Discord.
+		let infoEmbed = client.utils.embed('Информация о боте',
+				`${client.emoji.b} Lazy Cat — универсальный бот для вашего сервера Discord.
 				${client.emoji.cloud} Серверов: **${client.guilds.cache.size}**
 				${client.emoji.users} Пользователей: **${members}**
 				${client.emoji.cmd} Команд обработано: **${stats.commands}**
@@ -26,9 +20,7 @@ module.exports.run = async (client, interaction) => {
 				[**Добавить на свой сервер**](${client.config.inviteLink})
 
 				[Условия использования](${client.config.termsLink})
-				[Политика конфиденциальности](${client.config.privacyLink})`)
-			.setTimestamp()
-			.setFooter({ text: user.tag, iconURL: user.displayAvatarURL({dynamic: true}) })
+				[Политика конфиденциальности](${client.config.privacyLink})`, user)
 		interaction.reply({embeds: [infoEmbed]})
 	} catch (error) {
 		client.logger.log(error, 'err')

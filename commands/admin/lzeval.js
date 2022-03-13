492574f1be8fea42d6c9ce06dfc7eac63dccfc38
Dan <dan@dminc.ru>
2022-03-13 19:55:25 +0300

    Extend class Command

--- commands/admin/lzeval.js
+++ commands/admin/lzeval.js
@@ -1,52 +1,59 @@
 const beautify = require('js-beautify').js;
-module.exports.run = async (client, message, args) => {
-	if(message.author.id != '561822820632494081')
+const Command = require('../../class/Command')
+class Lzeval extends Command {
+	constructor(client) {
+		super(client, {
+			name: 'lzeval',
+			permissions: ['developer'],
+			type: 'message',
+			enabled: true,
+			guildOnly: false
+		})
+	}
+
+	async run (client, message, args) {
+		if(message.author.id != '561822820632494081')
 		return;
-	const code = args.join(" ");
-	const result = new Promise((resolve) => resolve(eval(code)));
-	return result.then((output) => {
-		if (typeof output !== "string") {
-			output = require('util').inspect(output, { depth: 0 });
-		}
-		output.replaceAll(client.config.token, "[REDACTED]")
-		output.replaceAll(client.config.webhookBan.id, "[REDACTED]")
-		output.replaceAll(client.config.webhookBan.token, "[REDACTED]")
-		output.replaceAll(client.config.secretKey, "[REDACTED]")
-		if (output.length > (1024 - (message.content.length))) {
-			client.logger.log(`Evaluated code via /lzeval`, 'debug')
-			return console.log(output)
-		}
-		let embedEval = client.utils.embed(undefined, undefined, message.author)
-			.addField(':inbox_tray: Input:', `\`\`\`js\n${beautify(code)}\n\`\`\``, false)
-			.addField(':outbox_tray: Output:', `\`\`\`js\n${output}\n\`\`\``, false)
-      	message.channel.send({embeds: [embedEval]});
-	}).catch((err) => {
-		err = err.toString()
-		if (err.includes(client.config.token)) {
-			err.replaceAll(client.config.token, "[REDACTED]")
-		}
-		if (err.includes(client.config.webhookBanToken)) {
-			err.replaceAll(client.config.webhookBanToken, "[REDACTED]")
-		}
-		if (err.includes(client.config.secretKey)) {
-			err.replaceAll(client.config.secretKey, "[REDACTED]")
-		}
-		if (err.length > (1024 - (message.content.length))) {
-			client.logger.log(`Evaluated code via /lzeval`, 'debug')
-			client.logger.log(`Error!`, 'err')
-			return console.error(output)
-		}
-		let embedEvalErr = client.utils.embed(undefined, undefined, message.author)
-			.addField(':inbox_tray: Input:', `\`\`\`js\n${beautify(code)}\n\`\`\``, false)
-			.addField(':outbox_tray: Output:', `\`\`\`js\n${err}\n\`\`\``, false)
-      	message.channel.send({embeds: [embedEvalErr]});
-	})
+		const code = args.join(" ");
+		const result = new Promise((resolve) => resolve(eval(code)));
+		return result.then((output) => {
+			if (typeof output !== "string") {
+				output = require('util').inspect(output, { depth: 0 });
+			}
+			output.replaceAll(client.config.token, "[REDACTED]")
+			output.replaceAll(client.config.webhookBan.id, "[REDACTED]")
+			output.replaceAll(client.config.webhookBan.token, "[REDACTED]")
+			output.replaceAll(client.config.secretKey, "[REDACTED]")
+			if (output.length > (1024 - (message.content.length))) {
+				client.logger.log(`Evaluated code via /lzeval`, 'debug')
+				return console.log(output)
+			}
+			let embedEval = client.utils.embed(undefined, undefined, message.author)
+				.addField(':inbox_tray: Input:', `\`\`\`js\n${beautify(code)}\n\`\`\``, false)
+				.addField(':outbox_tray: Output:', `\`\`\`js\n${output}\n\`\`\``, false)
+			message.channel.send({embeds: [embedEval]});
+		}).catch((err) => {
+			err = err.toString()
+			if (err.includes(client.config.token)) {
+				err.replaceAll(client.config.token, "[REDACTED]")
+			}
+			if (err.includes(client.config.webhookBanToken)) {
+				err.replaceAll(client.config.webhookBanToken, "[REDACTED]")
+			}
+			if (err.includes(client.config.secretKey)) {
+				err.replaceAll(client.config.secretKey, "[REDACTED]")
+			}
+			if (err.length > (1024 - (message.content.length))) {
+				client.logger.log(`Evaluated code via /lzeval`, 'debug')
+				client.logger.log(`Error!`, 'err')
+				return console.error(output)
+			}
+			let embedEvalErr = client.utils.embed(undefined, undefined, message.author)
+				.addField(':inbox_tray: Input:', `\`\`\`js\n${beautify(code)}\n\`\`\``, false)
+				.addField(':outbox_tray: Output:', `\`\`\`js\n${err}\n\`\`\``, false)
+			message.channel.send({embeds: [embedEvalErr]});
+		})
+	}
 }
 
-
-
-module.exports.data = {
-	name: "lzeval",
-	permissions: ["developer"],
-	type: "message"
-}
+module.exports = Lzeval;
--- commands/admin/lzunban.js
+++ commands/admin/lzunban.js
@@ -1,46 +1,53 @@
-const { MessageEmbed, WebhookClient } = require("discord.js");
-module.exports.run = async (client, message, args) => {
-	try {
-		
-		const webhookBan = new WebhookClient({id: client.config.webhookBan.id, token: client.config.webhookBan.token});
-		let noUser = client.utils.error('Не смог получить ID пользователя.', message.author)
-		try{
-			var user = await client.users.fetch((message.mentions.users.first() || args[0]));
-		} catch (error) {
-			return message.channel.send({embeds: [noUser]})
-		}
-		let userdb = await client.db.getUser(user.id);
+const { WebhookClient } = require("discord.js");
+const Command = require('../../class/Command')
+class Lzunban extends Command {
+	constructor(client) {
+		super(client, {
+			name: 'lzunban',
+			permissions: ['lia'],
+			type: 'message',
+			enabled: true,
+			guildOnly: false
+		})
+	}
 
-
-		let bannedAlready = client.utils.error('Пользователь не заблокирован.', message.author)
-		if (!userdb) return message.channel.send({embeds: [noUser]});
-		if (userdb.banned == false) {
-			return message.channel.send({embeds: [bannedAlready]})
-		}
-		let reasonUnban = message.content.slice(2) || `Рассмотрение апелляции`
-		await client.db.changeUser(user.id, 'banned', 0);
-		let lazyBan = client.utils.embed('Глобальная разблокировка', `Пользователь ${user.tag} успешно разблокирован в системе Lazy Cat.`, message.author)
-		message.channel.send({embeds: [lazyBan]});
-		let newUnban = client.utils.embed('Пользователь разблокирован', 'Пользователю предоставлен доступ к функциям', message.author)
-			.addField(`Причина`, `${reasonUnban}`, false)
-			.addField(`Пользователь`, `${user.tag} (${user.id})`, true)
-			.addField(`Модератор`, `${message.author.tag} (${message.author.id})`, true)
-		webhookBan.send({embeds: [newUnban]});
-		let lazyUnbanUser = client.utils.embed('Глобальная разблокировка', '**Вы разблокированы в системе Lazy Cat.** Это означает, что вам предоставлен доступ к функциям Lazy Cat.')
-			.addField(`Причина`, `${reasonUnban}`, true)
-		try{
-			await client.users.fetch(user.id).send({embeds: [lazyUnbanUser]});
+	async run (client, message, args) {
+		try {
+			const webhookBan = new WebhookClient({id: client.config.webhookBan.id, token: client.config.webhookBan.token});
+			let noUser = client.utils.error('Не смог получить ID пользователя.', message.author)
+			try{
+				var user = await client.users.fetch((message.mentions.users.first() || args[0]));
+			} catch (error) {
+				return message.channel.send({embeds: [noUser]})
+			}
+			let userdb = await client.db.getUser(user.id);
+			let bannedAlready = client.utils.error('Пользователь не заблокирован.', message.author)
+			if (!userdb) return message.channel.send({embeds: [noUser]});
+			if (userdb.banned == false) {
+				return message.channel.send({embeds: [bannedAlready]})
+			}
+			let reasonUnban = message.content.slice(2) || `Рассмотрение апелляции`
+			await client.db.changeUser(user.id, 'banned', 0);
+			let lazyBan = client.utils.embed('Глобальная разблокировка', `Пользователь ${user.tag} успешно разблокирован в системе Lazy Cat.`, message.author)
+			message.channel.send({embeds: [lazyBan]});
+			let newUnban = client.utils.embed('Пользователь разблокирован', 'Пользователю предоставлен доступ к функциям', message.author)
+				.addField(`Причина`, `${reasonUnban}`, false)
+				.addField(`Пользователь`, `${user.tag} (${user.id})`, true)
+				.addField(`Модератор`, `${message.author.tag} (${message.author.id})`, true)
+			webhookBan.send({embeds: [newUnban]});
+			let lazyUnbanUser = client.utils.embed('Глобальная разблокировка', '**Вы разблокированы в системе Lazy Cat.** Это означает, что вам предоставлен доступ к функциям Lazy Cat.')
+				.addField(`Причина`, `${reasonUnban}`, true)
+			try{
+				await client.users.fetch(user.id).send({embeds: [lazyUnbanUser]});
+			} catch (error) {
+				return;
+			};
 		} catch (error) {
-			return;
-		};
-	} catch (error) {
-		client.logger.log(error, 'err')
-		console.error(error)
-		message.channel.send({ content: `Произошла ошибка при выполнении команды.`})
+			client.logger.log(error, 'err')
+			console.error(error)
+			message.channel.send({ content: `Произошла ошибка при выполнении команды.`})
+		}
 	}
 }
-module.exports.data = {
-	name: "lzunban",
-	permissions: ["lia"],
-	type: "message"
-}
+
+module.exports = Lzunban;

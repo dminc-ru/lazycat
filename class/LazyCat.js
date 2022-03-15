5c1a4a5341103574ad56a3070193135e77f2b3f8
Dan <dan@dminc.ru>
2022-03-15 20:02:20 +0300

    Fixed loading command

--- class/LazyCat.js
+++ class/LazyCat.js
@@ -16,52 +16,52 @@ class LazyCat extends Client {
 			leaveOnEmpty: false,
 			enableLive: true
 		});
-        this.config = require('./config');
+        this.config = require('../config');
         this.commands = new Collection();
-        this.logger = require('./utils/logger');
-        this.db = require('./utils/db');
-        this.emoji = require('./emojis');
-        this.messages = require('./messages');
-        this.utils = require('./utils/utils')
+        this.permissions = new Collection();
+        this.logger = require('../utils/logger');
+        this.db = require('../utils/db');
+        this.emoji = require('../emojis');
+        this.messages = require('../messages');
+        this.utils = require('../utils/utils');
         this.queue = new Map();
         this.json = {
-            badges: this.badges = require(`${this.config.jsonPath}badges.json`),
-            bans: this.bans = require(`${this.config.jsonPath}bans.json`),
-            cases: this.cases = require(`${this.config.jsonPath}cases.json`),
-            config: this.config = require(`${this.config.jsonPath}config.json`),
-            daystrike: this.daystrike = require(`${this.config.jsonPath}daystrike.json`),
-            exchange: this.exchange = require(`${this.config.jsonPath}exchange.json`),
-            inventory: this.inventory = require(`${this.config.jsonPath}inventory.json`),
-            items: this.items = require(`${this.config.jsonPath}items.json`),
-            levels: this.items = require(`${this.config.jsonPath}levels.json`),
-            messages: this.items = require(`${this.config.jsonPath}messages.json`),
-            mutes: this.items = require(`${this.config.jsonPath}mutes.json`),
-            playlists: this.items = require(`${this.config.jsonPath}playlists.json`),
-            stats: this.stats = require(`${this.config.jsonPath}stats.json`),
-            shop: this.shop = require(`${this.config.jsonPath}shop.json`),
-            weekstrike: this.weekstrike = require(`${this.config.jsonPath}weekstrike.json`),
-            works: this.works = require(`${this.config.jsonPath}works.json`)
+            badges: require(`${this.config.jsonPath}badges.json`),
+            bans: require(`${this.config.jsonPath}bans.json`),
+            cases: require(`${this.config.jsonPath}cases.json`),
+            config: require(`${this.config.jsonPath}config.json`),
+            daystrike: require(`${this.config.jsonPath}daystrike.json`),
+            exchange: require(`${this.config.jsonPath}exchange.json`),
+            inventory: require(`${this.config.jsonPath}inventory.json`),
+            items: require(`${this.config.jsonPath}items.json`),
+            levels: require(`${this.config.jsonPath}levels.json`),
+            messages: require(`${this.config.jsonPath}messages.json`),
+            mutes: require(`${this.config.jsonPath}mutes.json`),
+            playlists: require(`${this.config.jsonPath}playlists.json`),
+            stats: require(`${this.config.jsonPath}stats.json`),
+            shop: require(`${this.config.jsonPath}shop.json`),
+            weekstrike: require(`${this.config.jsonPath}weekstrike.json`),
+            works: require(`${this.config.jsonPath}works.json`)
         }
     };
     async saveJSON (name, db) {
         fs.writeFileSync(`${this.config.jsonPath}${name}.json`, JSON.stringify(db, null, "\t"));
     }
-
     async LazyLoader () {
-        const eventFiles = fs.readdirSync('./events').filter(file => file.endsWith('.js')); // чтение папки events
+        const eventFiles = fs.readdirSync('./events/').filter(file => file.endsWith('.js')); // чтение папки events
         this.logger.log(`[!] DISCORD EVENTS`, "log")
         for (const file of eventFiles) {
-            const event = require(`./events/${file}`);
+            const event = require(`../events/${file}`);
             let eventName = file.split(".")[0];
             this.logger.log(`[!] Загружено событие ${file}`, "log")
             this.on(eventName, event.bind(null, this));
-            delete require.cache[require.resolve(`./events/${file}`)];
+            delete require.cache[require.resolve(`../events/${file}`)];
         };
         this.logger.log(`[!] COMMANDS`, "log")
         fs.readdirSync("./commands/").forEach(dirs => {
             const allCommands = fs.readdirSync(`./commands/${dirs}`).filter(files => files.endsWith('.js'));
             for (const file of allCommands) {
-                let command = require(`./commands/${dirs}/${file}`);
+                let command = new (require(`../commands/${dirs}/${file}`))(this);
                 this.logger.log(`[!] Загружена команда ${file}`, "log")
                 this.commands.set(command.data.name, command);
                 command.data.permissions.forEach(permission => {

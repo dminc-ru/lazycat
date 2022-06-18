28a06074b8516af2b82b8204727e33ffcd4304ad
Dan <dan@dminc.ru>
2022-06-18 18:42:15 +0300

    Added additional info

--- commands/economy/передать.js
+++ commands/economy/передать.js
@@ -25,15 +25,15 @@ class Pay extends Command {
 				return interaction.reply({content: "Вы не можете передать рыбки самому себе.", ephemeral: true})
 			if(!memberdb)
 				return interaction.reply({content: "Указанный пользователь не зарегистрирован в системе Lazy Cat.", ephemeral: true})
-			if(money > 10000)
-				return interaction.reply({content: `Вы можете перевести максимум 10000 ${client.emoji.fish} за одну транзакцию.`})
 			if( (money == 0) || (money < 0) )
 				return interaction.reply({content: "Укажите корректное количество рыбок.", ephemeral: true})
-			if(userdb.balance_fish < money)
-				return interaction.reply({content: "У вас недостаточно средств.", ephemeral: true})
+			if(userdb.balance_fish < money) {
+				let noMoney = client.utils.error(`У вас недостаточно средств.\nВаш баланс: ${userdb.balance_fish} ${client.emoji.fish}`)
+				return interaction.reply({content: `У вас недостаточно средств.`, ephemeral: true})
+			}
 			await client.db.changeUser(interaction.member.user.id, 'balance_fish', (userdb.balance_fish - money))
 			await client.db.changeUser(memberdb.discord_id, 'balance_fish', (memberdb.balance_fish + money))
-			let successTransaction = client.utils.success(`Успешно! Переведено ${money} ${client.emoji.fish}`, user)
+			let successTransaction = client.utils.success(`Успешно! Переведено ${money} ${client.emoji.fish}.\nВаш баланс: ${userdb.balance_fish - money} ${client.emoji.fish}`, user)
 			return interaction.reply({embeds: [successTransaction]})
 		} catch (error) {
 			client.logger.log(error, 'err')

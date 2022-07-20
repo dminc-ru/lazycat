a1c9273a5c1fa758356c7e0cff68769353f50ed4
Dan <dan@dminc.ru>
2022-07-20 20:53:26 +0300

    Add messages and update lzban messages

--- commands/admin/lzban.js
+++ commands/admin/lzban.js
@@ -14,7 +14,7 @@ class Lzban extends Command {
 	async run (client, message, args) {
 		try {
 			const webhookBan = new WebhookClient({id: client.config.webhookBan.id, token: client.config.webhookBan.token})
-			let noUser = client.utils.error('Пользователь не найден в базе данных.', message.author)
+			let noUser = client.utils.error(client.messages.userNotFound, message.author)
 			try {
 				var user = await client.users.cache.get((message.mentions.users.first() || args[0])) || await client.users.fetch((message.mentions.users.first() || args[0]));
 			} catch (error) {
@@ -23,23 +23,23 @@ class Lzban extends Command {
 			let userdb = await client.db.getUser(user.id);
 			if (!userdb) return message.channel.send({embeds: [noUser]});
 			if (userdb.banned == true) {
-				let bannedAlready = client.utils.error('Пользователь уже заблокирован.', message.author)
+				let bannedAlready = client.utils.error(client.messages.userAlreadyBanned, message.author)
 				return message.channel.send({embeds: [bannedAlready]})
 			}
-			let reasonBan = message.content.slice(2) || `Нарушение Пользовательского соглашения`
+			let reasonBan = message.content.slice(2) || client.messages.userAgreementViolation
 			await client.db.changeUser(user.id, 'banned', 1);
 			let lazyBan = client.utils.success(`Пользователь ${user.tag} успешно заблокирован в системе Lazy Cat.`)
 			message.channel.send({embeds: [lazyBan]});
-			let newBan = client.utils.embed('Пользователь заблокирован', 'Пользователю ограничен доступ к функциям')
-				.addField(`Причина`, `${reasonBan}`, false)
-				.addField(`Срок`, `навсегда`, false)
-				.addField(`Пользователь`, `${user.tag} (${user.id})`, true)
-				.addField(`Модератор`, `${message.author.tag} (${message.author.id})`, true)
+			let newBan = client.utils.embed(client.messages.userBlocked, client.messages.userFunctionsPaused)
+				.addField(client.messages.reasonCapital, `${reasonBan}`, false)
+				.addField(client.messages.blocktimeCapital, client.messages.forever, false)
+				.addField(client.messages.userCapital, `${user.tag} (${user.id})`, true)
+				.addField(client.messages.moderatorCapital, `${message.author.tag} (${message.author.id})`, true)
 			webhookBan.send({embeds: [newBan]});
 			let lazyBanUser = client.utils.embed('Глобальная блокировка', client.messages.userBlocked)
-				.addField(`Причина`, `${reasonBan}`, false)
-				.addField(`Срок`, `навсегда`, true)
-				.addField(`Модератор`, `${message.author.id}`, true)
+				.addField(client.messages.reasonCapital, `${reasonBan}`, false)
+				.addField(client.messages.blocktimeCapital, `навсегда`, true)
+				.addField(client.messages.moderatorCapital, `${message.author.id}`, true)
 			try {
 				await client.users.fetch(user.id).send({embeds: [lazyBanUser]});
 			} catch(error) {
@@ -48,7 +48,7 @@ class Lzban extends Command {
 		} catch (error) {
 			client.logger.log(error, 'err')
 			console.error(error)
-			message.channel.send({ content: `Произошла ошибка при выполнении команды.`})
+			message.channel.send({ content: client.messages.commandError })
 		}
 	}
 }

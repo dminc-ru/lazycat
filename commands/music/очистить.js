ee6a3bb7d87bd753e6df01e484bfe4523cd4ebc5
Dan <dan@dminc.ru>
2022-11-20 23:43:52 +0300

    Fixed typos

--- commands/music/очистить.js
+++ commands/music/очистить.js
@@ -15,9 +15,8 @@ class Clear extends Command {
         await interaction.deferReply();
         var user = client.users.cache.get(interaction.member.user.id)
         const queue = client.player.getQueue(interaction.guildId);
-        let noMusic = client.utils.embed('Очередь пуста', undefined, user)
+        let noMusic = client.utils.embed('Очередь пуста.', undefined, user)
         if (!queue) return void interaction.followUp({ embeds: [noMusic] });
-            
         queue.clear();
         let cleared = client.utils.embed('Очередь очищена.', undefined, user)
         interaction.followUp({ embeds: [cleared] });
